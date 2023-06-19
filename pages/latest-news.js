043104e92b7adb6b987232751cddff139b8fbac2
MuhammadZain12 <uuu81201@gmail.com>
2023-06-19 23:10:44 +0500

    Latest News Page Done Needed to be reviewed

--- pages/latest-news.js
+++ pages/latest-news.js
@@ -1,18 +1,25 @@
 import Navbar from "@/components/Navbar";
 import InfoBar from "@/components/InfoBar";
 import News from "@/components/News";
+import classes from "@/styles/scroll.module.css";
 
 const NewsPage = () => {
   return (
     <div className="w-screen relative min-h-screen bg-no-repeat bg-cover bg-[url('/images/landing-page/landing-page-bg.png')] h-screen">
-      <Navbar className={`pt-4 mb-9.5`}/>
-      <InfoBar/>
+      <Navbar className={`pt-4 mb-9.5`} />
+      <InfoBar />
       <div className="relative flex flex-row px-19.7 pt-12.7 pb- h-[70%] w-full">
-        <div className="bg-news-box px-7.7 overflow-scroll overflow-x-hidden">
-          <News/>
+        <div
+          className={`${classes.latestNews} px-7.7 overflow-scroll overflow-x-hidden`}
+        >
+          <div className="bg-news-box">
+            <News />
+          </div>
         </div>
       </div>
-      <div className="absolute right-0 bottom-2 bg-bottom-note text-white pr-6 text-xs pl-32">免責聲明</div>
+      <div className="absolute right-0 bottom-2 bg-bottom-note text-white pr-6 text-xs pl-32">
+        免責聲明
+      </div>
     </div>
   );
 };
