9954d1b5a3a1e12b5a37dd96149d21fc3044d5b8
abubakarafzal444 <abubakarafzal444@gmail.com>
2023-06-19 12:44:45 +0500

    removed carousel

---
+++ components/CustomSlider.jsx
@@ -0,0 +1,47 @@
+// import React from "react";
+// import { Slide } from "react-slideshow-image";
+// import "react-slideshow-image/dist/styles.css";
+
+// const CustomSlider = ({ verticalBullets }) => {
+//   return (
+//     <div
+//       className={`${
+//         verticalBullets ? "vertical-bullets rotate-90" : "horizontal-bullets"
+//       }`}
+//     >
+//       <Slide
+//         slidesToScroll={1}
+//         slidesToShow={1}
+//         indicators={() => (
+//           <div className="w-3 h-3 rounded-full border-2 border-white bg-white bg-transparent indicator mx-1.9 "></div>
+//         )}
+//         arrows={false}
+//       >
+//         <div
+//           style={{
+//             textAlign: "center",
+//             background: "red",
+//             padding: "200px 0",
+//             fontSize: "30px",
+//             rotate: "90deg",
+//           }}
+//         >
+//           First Slide
+//         </div>
+//         <div
+//           style={{
+//             textAlign: "center",
+//             background: "green",
+//             padding: "200px 0",
+//             fontSize: "30px",
+//             rotate: "90deg",
+//           }}
+//         >
+//           Second Slide
+//         </div>
+//       </Slide>
+//     </div>
+//   );
+// };
+
+// export default CustomSlider;
--- pages/index.js
+++ pages/index.js
@@ -1,94 +1,15 @@
 import React from "react";
 import InfoBar from "@/components/InfoBar";
 import Navbar from "@/components/Navbar";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
+// import CustomSlider from "@/components/CustomSlider";
 
 const LandingPage = () => {
   return (
-    <div className="w-screen min-h-screen bg-no-repeat bg-cover bg-[url('/images/landing-page/landing-page-bg.png')]">
+    <div className="w-screen min-h-screen bg-no-repeat bg-cover bg-[url('/images/landing-page/landing-page-bg.png')] h-screen">
       <Navbar className="pt-4 mb-9.5" />
       <InfoBar />
-      <div className="relative max-w-[600px] w-[34%]">
-        <Slide
-          slidesToScroll={1}
-          slidesToShow={1}
-          indicators={(index) => (
-            <div className="indicator w-3 h-3 rounded-full border-2 border-white bg-white"></div>
-          )}
-          arrows={false}
-        >
-          <div
-            style={{
-              textAlign: "center",
-              background: "red",
-              padding: "200px 0",
-              fontSize: "30px",
-            }}
-          >
-            First Slide
-          </div>
-          <div
-            style={{
-              textAlign: "center",
-              background: "orange",
-              padding: "200px 0",
-              fontSize: "30px",
-            }}
-          >
-            Second Slide
-          </div>
-          <div
-            style={{
-              textAlign: "center",
-              background: "yellow",
-              padding: "200px 0",
-              fontSize: "30px",
-            }}
-          >
-            Third Slide
-          </div>
-          <div
-            style={{
-              textAlign: "center",
-              background: "green",
-              padding: "200px 0",
-              fontSize: "30px",
-            }}
-          >
-            Fourth Slide
-          </div>
-          <div
-            style={{
-              textAlign: "center",
-              background: "blue",
-              padding: "200px 0",
-              fontSize: "30px",
-            }}
-          >
-            Sixth Slide
-          </div>
-          <div
-            style={{
-              textAlign: "center",
-              background: "indigo",
-              padding: "200px 0",
-              fontSize: "30px",
-            }}
-          >
-            Seventh Slide
-          </div>
-          <div
-            style={{
-              textAlign: "center",
-              background: "violet",
-              padding: "200px 0",
-              fontSize: "30px",
-            }}
-          >
-            Eight Slide
-          </div>
-        </Slide>
+      <div className="relative w-[54%] h-[43%] bg-black bg-opacity-[0.15] ml-19">
+        {/* <CustomSlider verticalBullets={true} /> */}
       </div>
     </div>
   );
