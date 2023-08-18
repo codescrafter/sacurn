cc464f32aaa1627cc261ba7987e7178244b5cd5a
aliraza2u <raza8r@gmail.com>
2023-08-18 05:33:26 +0500

    design product details left side

--- src/components/Products/Details.tsx
+++ src/components/Products/Details.tsx
@@ -10,20 +10,44 @@ const Details = () => {
       {/* First Col */}
       <div className="w-[38%]">
         <ImgSlider images={['/images/products/green/detail1.png', '/images/products/green/bird.png']} />
-        <div>
-          <h5 className="text-lg font-bold text-white ">Price</h5>
-          <div className="bg-transparent-grey rounded p-4 flex justify-center items-center">
-            <div className="flex justify-evenly items-start">
-              <div className="flex items-baseline text-white">
-                <span className="text-2xl">USD</span>
-                <b className="text-[50px] font-semibold">5.00~5.00</b>
-                <span>/Tonne</span>
+        <div className="pl-5 pt-7">
+          {/* Price */}
+          <h5 className="text-lg font-bold text-white mb-2 pl-3">Price</h5>
+          <div className="bg-transparent-grey rounded p-5 flex justify-center items-center">
+            <div className="flex justify-between items-center gap-4">
+              <div>
+                <div className="flex items-baseline text-white">
+                  <span className="text-2xl">USD</span>
+                  <b className="text-5xl font-semibold">5.00~5.00</b>
+                  <span className="text-2xl">/Tonne</span>
+                </div>
+                <p className="text-sm text-white">Ranges from vintage 2017</p>
               </div>
               <div>
                 <img src="/images/products/green/dollar-2.svg" alt="sacurn" />
               </div>
             </div>
-            <p className="text-sm">Ranges from vintage 2017</p>
+          </div>
+          {/* Ratings Breakdown */}
+          <div className="mt-4">
+            <h5 className="text-lg font-bold text-white mb-2 pl-3">Ratings Breakdown</h5>
+            <div className="grid grid-cols-2 gap-6">
+              <div className="bg-transparent-grey rounded flex flex-col justify-center items-center p-4">
+                <p className="text-xl font-bold text-white">Carbon Rating</p>
+                <p className="text-xl font-bold text-white">A</p>
+              </div>
+              <div className="bg-transparent-grey rounded flex flex-col justify-center items-center p-4">
+                <p className="text-xl font-bold text-white">Co-benefit</p>
+                <p className="text-xl font-bold text-white">4/5</p>
+              </div>
+            </div>
+          </div>
+          {/* Certification From */}
+          <div className="mt-4">
+            <h5 className="text-lg font-bold text-white mb-2 pl-3">Certification From</h5>
+            <div className="bg-transparent-grey rounded p-8 flex justify-center items-center">
+              <img src="/images/products/green/verra-standard.svg" alt="sacurn" />
+            </div>
           </div>
         </div>
       </div>
@@ -60,8 +84,8 @@ const ImgSlider = ({ images }: SliderIProps) => {
     afterChange: (currSlide: number) => setCurrentSlide(currSlide)
   };
   return (
-    <div className="relative h-full">
-      <div className="absolute z-20 left-0 bottom-4 right-0 gap-1.5 flex pr-[30%] pl-6">
+    <div className="relative h-[312px]">
+      <div className="absolute z-20 left-0 bottom-12 right-0 gap-1.5 flex pr-[30%] pl-6">
         {['01', '02', '03'].map((item, index) => (
           <div
             key={index}
