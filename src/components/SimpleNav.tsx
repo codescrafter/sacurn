65f1e0c27a5014088548c817f4b6772b24e361c4
MuhammadZain12 <uuu81201@gmail.com>
2023-08-26 06:55:43 +0500

    issues resolved

--- src/components/SimpleNav.tsx
+++ src/components/SimpleNav.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 const SimpleNav = ({ className }: IProps) => {
   return (
-    <div className={classNames('bg-nFavy-blue flex flex-col h-16 justify-center', className)}>
+    <div className={classNames('bg-navy-blue flex flex-col h-16 justify-center', className)}>
       <div className="flex flex-row ml-10">
         <img src="/images/navbar/sacurn-logo.svg" width={192} height={39} alt="sacurn" />
         <div className="h-[45%] mx-4 bg-white w-0.3 self-end mb-2" />
