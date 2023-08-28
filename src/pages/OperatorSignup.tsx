358636dd083ddb0d2a88283fa27874f5104f7d07
MuhammadZain12 <uuu81201@gmail.com>
2023-08-28 13:40:07 +0500

    signup page done

--- src/pages/OperatorSignup.tsx
+++ src/pages/OperatorSignup.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+import { useState } from 'react';
 const OperatorSignUp = () => {
   return (
     <div className="flex flex-row h-screen">
@@ -10,7 +12,7 @@ const OperatorSignUp = () => {
           Adventure Comes with you! Join With Us!
         </p>
       </div>
-      <div className="flex flex-col w-[70vw] min-[1550px]:px-22.5 min-[1200px]:px-18 px-13 min-[1550px]:pt-20 pt-14">
+      <div className="flex flex-col w-[70vw] min-[1550px]:px-21.7 min-[1200px]:px-18 px-13 min-[1550px]:pt-20 pt-14">
         <h1 className="min-[1550px]:text-5.2xl min-[1200px]:text-4xl text-3xl text-center text-navy-blue font-semibold  min-[1550px]:mb-8.2 min-[1200px]:mb-6 mb-5">
           Account sign up
         </h1>
@@ -21,37 +23,54 @@ const OperatorSignUp = () => {
           placeholder="艾克斯厚定股份有限公司"
           className={`${Style} w-full min-[1550px]:mb-10.7 min-[1200px]:mb-8 mb-6`}
         />
-        <div className="flex flex-row justify-between w-full min-[1550px]:mb-10.7 min-[1200px]:mb-8 mb-6">
-          <div className="flex flex-col self-start w-[45%]">
-            <Heading heading={'帳號/email'} />
-            <input type="text" placeholder="maskholding＠gmail.com" className={`${Style} w-full`} />
-          </div>
-          <div className="flex flex-col self-end w-[45%]">
-            <Heading heading={'統編'} />
-            <input type="text" placeholder="12345678" className={`${Style} w-full`} />
-          </div>
+        <div className="flex flex-row justify-between w-full min-[1550px]:mb-6 min-[1200px]:mb-8 mb-6">
+          <Field heading="管理者姓名" type="text" className="self-start" />
+          <Field
+            heading="管理者手機"
+            type="text"
+            className="self-end"
+            downText="此欄位再多項服務提供時將進行驗證，請正確填寫"
+          />
         </div>
-        <Heading heading={'使用者姓名'} />
-        <input type="text" className={`${Style} w-full min-[1550px]:mb-10.7 min-[1200px]:mb-8 mb-6`} />
+        <div className="flex flex-row justify-between w-full min-[1550px]:mb-6 min-[1200px]:mb-8 mb-6">
+          <Field
+            heading="帳號"
+            type="text"
+            className="self-start"
+            downText="英數混合，最常請勿超過20個字元，可接受 ”_” “.” “@” 三種符號"
+          />
+          <Field heading="Email" type="email" className="self-start" />
+        </div>
         <div className="flex flex-row justify-between w-full min-[1550px]:mb-7.2 min-[1200px]:mb-6 mb-5">
           <div className="flex flex-col self-start w-[45%]">
-            <Heading heading={'密碼'} />
-            <input type="text" className={`${Style} w-full`} />
-          </div>
-          <div className="flex flex-col self-end w-[45%]">
             <Heading heading={'再次輸入密碼'} />
-            <input type="text" className={`${Style} w-full`} />
+            <PasswordInput />
+            <p className="text-grey ml-5.5 mt-2 w-[100%] min-[1500px]:text-base min-[1200px]:text-sm text-[12px]">
+              提示：密碼需至少有12字元，請混和使用大小寫字母、數字，使密碼更加安全。
+            </p>
+          </div>
+          <div className="flex flex-col self-start w-[45%]">
+            <Heading heading={'再次輸入密碼'} />
+            <PasswordInput />
+            <div className="flex ml-5.5 mt-2 w-[100%] justify-start gap-1">
+              <input className="mt-1 self-start min-[1500px]:w-3.5 w-3 min-[1500px]:h-4 h-3.5" type="checkbox" />
+              <div>
+                <p className="text-navy-blue min-[1500px]:text-base min-[1200px]:text-sm text-[12px] font-bold">
+                  我已知悉並同意個人資料將被平台搜集、處理及利用。
+                </p>
+                <p className="text-bright-red min-[1500px]:text-xs min-[1200px]:text-xms text-xxs">
+                  請務必確認勾選此框，才能點選下一步。
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <p className=" text-black min-[1550px]:text-base min-[1200px]:text-sm text-[12px] font-semibold ml-5">
-          點擊「註冊」即表示你同意我們的《服務條款》、《隱私政策》和《Cookie政策》。
-        </p>
-        <p className="text-black min-[1550px]:text-base min-[1200px]:text-sm text-[12px] font-semibold ml-5">
-          你可能會收到我們的簡訊通知，而且可以隨時選擇停止接收。
-        </p>
-        <button className="rounded-full bg-operator-signup shadow-operator-signup-button min-[1550px]:text-xl min-[1200px]:text-base text-base font-bold w-full min-[1550px]:h-13.2 h-10 mt-3.2">
+        <button className="rounded-full bg-operator-signup shadow-operator-signup-button text-white min-[1550px]:text-xl min-[1200px]:text-base text-base font-bold w-full min-[1550px]:h-13.2 h-10 mt-3.2">
           註冊
         </button>
+        <p className=" text-dark-grey text-center min-[1550px]:text-base min-[1200px]:text-sm text-[12px] font-semibold mt-3 ml-5">
+          點擊「註冊」即表示你同意我們的《服務條款》、《隱私政策》和《Cookie政策》。
+        </p>
       </div>
     </div>
   );
@@ -59,11 +78,50 @@ const OperatorSignUp = () => {
 
 export default OperatorSignUp;
 
-const Heading = ({ heading }: { heading: string }) => {
+interface HeadingProps {
+  heading: string;
+}
+const Heading = ({ heading }: HeadingProps) => {
   return (
     <label className="text-navy-blue min-[1550px]:text-xl min-[1200px]:text-lg text-base font-semibold self-start ml-8 mb-2">
       {heading}
     </label>
+  );
+};
+
+interface FieldProps {
+  heading: string;
+  type: string;
+  className: string;
+  downText?: string;
+}
+
+const Field = ({ heading, type, className, downText }: FieldProps) => {
+  return (
+    <div className={classNames('relative flex flex-col w-[45%]', className)}>
+      <Heading heading={heading} />
+      <input type={type} className={classNames(Style, 'w-full')} />
+      <p className="text-grey ml-5.5 mt-2 w-[100%] min-[1500px]:text-base min-[1200px]:text-sm text-[12px]">
+        {downText}
+      </p>
+    </div>
+  );
+};
+
+const PasswordInput = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div
+      className={classNames(
+        'rounded-full shadow-operator-signup-input bg-white min-[1550px]:h-17.5 min-[1200px]:h-13.2 h-11.5 px-5 py-3 text-black min-[1550px]:text-xl min-[1200px]:text-lg text-base outline-none flex justify-between w-[100%]'
+      )}
+    >
+      <input className="outline-none w-[85%]" type={showPassword ? 'text' : 'password'} />
+      <img
+        src={showPassword ? '/images/operator-signup/visible.svg' : '/images/operator-signup/invisible.svg'}
+        onClick={() => setShowPassword((prev) => !prev)}
+      />
+    </div>
   );
 };
 
