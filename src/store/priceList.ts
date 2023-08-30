f03816e43569c78bfb0d2f4ff79820a54adfe293
eric.lin <seekingfor520@gmail.com>
2023-08-30 23:22:41 +0800

    Add priceList methods

---
+++ src/store/priceList.ts
@@ -0,0 +1,46 @@
+import { create } from 'zustand';
+
+import { Cart, Order } from '@/libs/api';
+import apiClient from '@/libs/api/client';
+
+import { ModalType, useModalStore } from './modal';
+
+type PriceListState = {
+  priceList: Order[];
+  getPriceList: (arg: { carbonCreditId?: string; desc?: string; page?: number; sortby?: string }) => void;
+  addToCart: (arg: Cart) => void;
+};
+
+export const usePriceListStore = create<PriceListState>((set) => ({
+  priceList: [],
+  getPriceList: async (arg) => {
+    const { carbonCreditId, desc, page, sortby } = arg;
+
+    try {
+      useModalStore.getState().open(ModalType.Loading);
+      const response = await apiClient.trade.tradeOrderSellList(carbonCreditId, desc, page, sortby);
+      set({ priceList: response.results });
+      useModalStore.getState().close();
+    } catch (error) {
+      set({ priceList: [] });
+      const err = error as Error;
+      console.error(err);
+      useModalStore.getState().open(ModalType.Error, {
+        errorText: `[${err.name}] ${err.message}`
+      });
+    }
+  },
+  addToCart: async (arg: Cart) => {
+    try {
+      useModalStore.getState().open(ModalType.Loading);
+      await apiClient.trade.tradeCartCreate(arg);
+      useModalStore.getState().close();
+    } catch (error) {
+      const err = error as Error;
+      console.error(err);
+      useModalStore.getState().open(ModalType.Error, {
+        errorText: `[${err.name}] ${err.message}`
+      });
+    }
+  }
+}));
