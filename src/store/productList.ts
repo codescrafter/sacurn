aea35ddce379bf4fca501dc52203364625fb67ac
eric.lin <seekingfor520@gmail.com>
2023-08-31 23:23:50 +0800

    Replace products to productList

--- src/store/productList.ts
+++ src/store/productList.ts
@@ -5,22 +5,21 @@ import apiClient from '@/libs/api/client';
 
 import { ModalType, useModalStore } from './modal';
 
-type ProductsState = {
-  products: CarbonCredit[];
+type ProductListState = {
+  productList: CarbonCredit[];
   getProducts: (arg: { desc?: string; sortby?: string; page?: number; tags?: string }) => void;
 };
 
-export const useProductsStore = create<ProductsState>((set) => ({
-  products: [],
+export const useProductsStore = create<ProductListState>((set) => ({
+  productList: [],
   getProducts: async (arg) => {
     const { desc, sortby, page, tags } = arg;
     try {
       useModalStore.getState().open(ModalType.Loading);
       const response = await apiClient.carbonCredit.carbonCreditList(desc, page, sortby, tags);
-      set({ products: response.results });
+      set({ productList: response.results });
       useModalStore.getState().close();
     } catch (error) {
-      set({ products: [] });
       const err = error as Error;
       console.error(err);
       useModalStore.getState().open(ModalType.Error, {
