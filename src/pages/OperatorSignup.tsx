62d6299f8c9780a76f718f2c7e7ae7d808079d94
Shawnz <53845962+shawn1126@users.noreply.github.com>
2023-09-07 00:22:59 +0800

    Update OperatorSignup.tsx
    add phoneNumber and lastName for new schema

--- src/pages/OperatorSignup.tsx
+++ src/pages/OperatorSignup.tsx
@@ -1,6 +1,51 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import classNames from 'classnames';
 import { useState } from 'react';
+import { useForm, UseFormRegister } from 'react-hook-form';
+// import { useNavigate } from 'react-router-dom';
+import * as yup from 'yup';
+
+import { useUserStore } from '@/store/user';
+
+type SignupFormType = {
+  username: string;
+  email: string;
+  password1: string;
+  password2: string;
+  phone: string;
+  lastname: string;
+};
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+const schema = yup
+  .object({
+    username: yup.string().required('username required'),
+    email: yup.string().email('email format should be correct').required('email required'),
+    password1: yup.string().required('password required'),
+    // match password1 and password2, show error if they dont match
+    password2: yup
+      .string()
+      .oneOf([yup.ref('password1')], 'Passwords must match')
+      .required('password required'),
+    phone: yup.string().matches(phoneRegExp, 'Phone number is not valid').required('phoneNumber required'),
+    lastname: yup.string().required('lastname required')
+  })
+  .required();
+
 const OperatorSignUp = () => {
+  const { register, handleSubmit, formState } = useForm<SignupFormType>({ resolver: yupResolver(schema) });
+  // const navigate = useNavigate();
+  const signup = useUserStore((state) => state.signup);
+
+  const onSubmit = handleSubmit(async (data) => {
+    // console.log('data being sent', data);
+    await signup(data);
+
+    // if (isSuccess) navigate('/login');
+  });
+
+  console.log('formState.errors', formState.errors);
   return (
     <div className="flex flex-row h-screen">
       <div className="flex flex-col items-center justify-center min-[1550px]:ml-23.5 min-[1200px]:ml-19 ml-14">
@@ -12,23 +57,29 @@ const OperatorSignUp = () => {
           Adventure Comes with you! Join With Us!
         </p>
       </div>
-      <div className="flex flex-col w-[70vw] min-[1550px]:px-21.7 min-[1200px]:px-18 px-13 min-[1550px]:pt-20 pt-14">
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col w-[70vw] min-[1550px]:px-21.7 min-[1200px]:px-18 px-13 min-[1550px]:pt-20 pt-14"
+      >
         <h1 className="min-[1550px]:text-5.2xl min-[1200px]:text-4xl text-3xl text-center text-navy-blue font-semibold  min-[1550px]:mb-8.2 min-[1200px]:mb-6 mb-5">
           Account sign up
         </h1>
-        <Heading heading="公司名稱" />
+        {/* <Heading heading="公司名稱" />
         <input
           id="co-name"
           type="text"
           placeholder="艾克斯厚定股份有限公司"
           className={classNames(Style, 'w-full min-[1550px]:mb-10.7 min-[1200px]:mb-8 mb-6')}
-        />
+        /> */}
         <div className="flex flex-row justify-between w-full min-[1550px]:mb-6 min-[1200px]:mb-8 mb-6">
-          <Field heading="管理者姓名" type="text" className="self-start" />
+          <Field heading="管理者姓名" type="text" className="self-start" register={register} id="lastname" />
+
           <Field
             heading="管理者手機"
             type="text"
             className="self-end"
+            register={register}
+            id="phone"
             downText="此欄位再多項服務提供時將進行驗證，請正確填寫"
           />
         </div>
@@ -37,21 +88,23 @@ const OperatorSignUp = () => {
             heading="帳號"
             type="text"
             className="self-start"
+            register={register}
+            id="username"
             downText="英數混合，最常請勿超過20個字元，可接受 ”_” “.” “@” 三種符號"
           />
-          <Field heading="Email" type="email" className="self-start" />
+          <Field heading="Email" type="email" className="self-start" register={register} id="email" />
         </div>
         <div className="flex flex-row justify-between w-full min-[1550px]:mb-7.2 min-[1200px]:mb-6 mb-5">
           <div className="flex flex-col self-start w-[45%]">
-            <Heading heading="再次輸入密碼" />
-            <PasswordInput />
+            <Heading heading="設定密碼" />
+            <PasswordInput register={register} id="password1" />
             <p className="text-grey ml-5.5 mt-2 w-[100%] min-[1500px]:text-base min-[1200px]:text-sm text-[12px]">
               提示：密碼需至少有12字元，請混和使用大小寫字母、數字，使密碼更加安全。
             </p>
           </div>
           <div className="flex flex-col self-start w-[45%]">
             <Heading heading="再次輸入密碼" />
-            <PasswordInput />
+            <PasswordInput register={register} id="password2" />
             <div className="flex ml-5.5 mt-2 w-[100%] justify-start gap-1">
               <input className="mt-1 self-start min-[1500px]:w-3.5 w-3 min-[1500px]:h-4 h-3.5" type="checkbox" />
               <div>
@@ -65,13 +118,16 @@ const OperatorSignUp = () => {
             </div>
           </div>
         </div>
-        <button className="rounded-full bg-operator-signup shadow-operator-signup-button text-white min-[1550px]:text-xl min-[1200px]:text-base text-base font-bold w-full min-[1550px]:h-13.2 h-10 mt-3.2">
+        <button
+          type="submit"
+          className="rounded-full bg-operator-signup shadow-operator-signup-button text-white min-[1550px]:text-xl min-[1200px]:text-base text-base font-bold w-full min-[1550px]:h-13.2 h-10 mt-3.2"
+        >
           註冊
         </button>
         <p className=" text-dark-grey text-center min-[1550px]:text-base min-[1200px]:text-sm text-[12px] font-semibold mt-3 ml-5">
           點擊「註冊」即表示你同意我們的《服務條款》、《隱私政策》和《Cookie政策》。
         </p>
-      </div>
+      </form>
     </div>
   );
 };
@@ -94,13 +150,21 @@ interface FieldProps {
   type: string;
   className: string;
   downText?: string;
+  register?: UseFormRegister<SignupFormType>;
+  id?: 'username' | 'email' | 'password1' | 'password2' | 'lastname' | 'phone';
+}
+interface PasswordInputProps {
+  className?: string;
+  register?: UseFormRegister<SignupFormType>;
+  id?: 'password1' | 'password2';
 }
 
-const Field = ({ heading, type, className, downText }: FieldProps) => {
+const Field = ({ heading, type, className, downText, register, id }: FieldProps) => {
+  const registeringObject = register && id ? { ...register(id) } : {};
   return (
     <div className={classNames('relative flex flex-col w-[45%]', className)}>
       <Heading heading={heading} />
-      <input type={type} className={classNames(Style, 'w-full')} />
+      <input disabled={!id} {...registeringObject} type={type} className={classNames(Style, 'w-full')} />
       <p className="text-grey ml-5.5 mt-2 w-[100%] min-[1500px]:text-base min-[1200px]:text-sm text-[12px]">
         {downText}
       </p>
@@ -108,11 +172,18 @@ const Field = ({ heading, type, className, downText }: FieldProps) => {
   );
 };
 
-const PasswordInput = () => {
+const PasswordInput = ({ className, register, id }: PasswordInputProps) => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const registeringObject = register && id ? { ...register(id) } : {};
+
   return (
     <div className="rounded-full shadow-operator-signup-input bg-white min-[1550px]:h-17.5 min-[1200px]:h-13.2 h-11.5 px-5 py-3 text-black min-[1550px]:text-xl min-[1200px]:text-lg text-base outline-none flex justify-between w-[100%]">
-      <input className="outline-none w-[85%]" type={showPassword ? 'text' : 'password'} />
+      <input
+        {...registeringObject}
+        className={classNames('outline-none w-[85%]', className)}
+        type={showPassword ? 'text' : 'password'}
+      />
       <img
         src={showPassword ? '/images/operator-signup/visible.svg' : '/images/operator-signup/invisible.svg'}
         onClick={() => setShowPassword((prev) => !prev)}
