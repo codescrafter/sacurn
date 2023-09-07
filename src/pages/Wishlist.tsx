2f0f72e96b9c78e25ae869ad78fcc863382de586
abubakarafzal444 <abubakarafzal444@gmail.com>
2023-09-07 23:56:16 +0500

    wishlist get and read

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
+
 import FilterBar from '@/components/FilterBar';
 import Navbar from '@/components/Navbar';
 import WishListProdCard from '@/components/WishListProdCard';
+import { useWishListStore } from '@/store/wishList';
 import { WishlistTypes } from '@/type';
 import { WISHLIST_DATA } from '@/util/constants';
 
 const WishList = () => {
+  const wishList = useWishListStore((state) => state.wishList);
+  const getWishList = useWishListStore((state) => state.getWishList);
+  console.log(wishList);
+
+  useEffect(() => {
+    console.log('fetching wishlist');
+    getWishList(1);
+  }, []);
   return (
     <div className="bg-[url('../public/images/wishlist/background.png')] bg-no-repeat bg-cover py-10 h-screen px-5 pr-8">
       <Navbar className="mb-8" />
