e1b4fe0debf80fe2ea201bab49bfcca0b2415299
abubakarafzal444 <abubakarafzal444@gmail.com>
2023-09-08 04:14:11 +0500

    get wishlist and delete wishlist

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -4,7 +4,7 @@ const FilterBar = () => {
       <div className="flex flex-row">
         <select
           name="location"
-          className="bg-drop-down rounded-xl min-[1500px]:text-mdlg text-mdbase font-bold bg-trans-grey min-[1500px]:w-[220px] min-[1200px]:w-[170px] w-[150px] min-[1500px]:h-[41px] h-[36px] outline-none px-[1.5rem] appearance-none bg-no-repeat bg-[center_right_1.5rem] min-[1500px]:mx-4 mx-2"
+          className="bg-drop-down rounded-xl min-[1500px]:text-mdlg text-mdbase font-bold bg-trans-grey min-[1500px]:w-[220px] min-[1200px]:w-[170px] w-[150px] min-[1500px]:h-[41px] h-[36px] outline-none px-[1.5rem] appearance-none bg-no-repeat bg-[center_right_1.5rem] min-[1500px]:mx-4 mx-2 text-white"
         >
           <option className="bg-grey hover:bg-black cursor-pointer">Location</option>
           <option className="bg-grey">Location</option>
@@ -12,7 +12,7 @@ const FilterBar = () => {
         </select>
         <select
           name="vintages"
-          className="bg-drop-down rounded-xl min-[1500px]:text-mdlg text-mdbase font-bold bg-trans-grey min-[1500px]:w-[220px] min-[1200px]:w-[170px] w-[150px] min-[1500px]:h-[41px] h-[36px] outline-none px-[1.5rem] appearance-none bg-no-repeat bg-[center_right_1.5rem] min-[1500px]:mx-4 mx-2"
+          className="bg-drop-down rounded-xl min-[1500px]:text-mdlg text-mdbase font-bold bg-trans-grey min-[1500px]:w-[220px] min-[1200px]:w-[170px] w-[150px] min-[1500px]:h-[41px] h-[36px] outline-none px-[1.5rem] appearance-none bg-no-repeat bg-[center_right_1.5rem] min-[1500px]:mx-4 mx-2 text-white"
         >
           <option>vintages</option>
           <option>vintages</option> <option>vintages</option>
@@ -21,15 +21,15 @@ const FilterBar = () => {
         </select>
         <select
           name="prices"
-          className="wishSelect bg-drop-down rounded-xl min-[1500px]:text-mdlg text-mdbase font-bold bg-trans-grey min-[1500px]:w-[220px] min-[1200px]:w-[170px] w-[150px] min-[1500px]:h-[41px] h-[36px] outline-none px-[1.5rem] appearance-none bg-no-repeat bg-[center_right_1.5rem] min-[1500px]:mx-4 mx-2"
+          className="wishSelect bg-drop-down rounded-xl min-[1500px]:text-mdlg text-mdbase font-bold bg-trans-grey min-[1500px]:w-[220px] min-[1200px]:w-[170px] w-[150px] min-[1500px]:h-[41px] h-[36px] outline-none px-[1.5rem] appearance-none bg-no-repeat bg-[center_right_1.5rem] min-[1500px]:mx-4 mx-2 text-white"
         >
           <option>Prices</option>
         </select>
       </div>
-      <label className="min-[1500px]:ml-22.5 min-[1200px]:ml-17 ml-11 min-[1500px]:text-base min-[1200px]:text-sm text-[12px]">
+      <label className="min-[1500px]:ml-22.5 min-[1200px]:ml-17 ml-11 min-[1500px]:text-base min-[1200px]:text-sm text-[12px] text-white">
         Sort:&nbsp;
       </label>
-      <span className="min-[1500px]:mr-8 mr-6">
+      <span className="min-[1500px]:mr-8 mr-6 text-white">
         <select
           id="sort"
           className="bg-transparent min-[1500px]:text-base min-[1200px]:text-sm text-[12px] outline-none"
--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -3,25 +3,23 @@ import { useEffect } from 'react';
 import FilterBar from '@/components/FilterBar';
 import Navbar from '@/components/Navbar';
 import WishListProdCard from '@/components/WishListProdCard';
+import { WatchList } from '@/libs/api';
 import { useWishListStore } from '@/store/wishList';
-import { WishlistTypes } from '@/type';
-import { WISHLIST_DATA } from '@/util/constants';
 
 const WishList = () => {
   const wishList = useWishListStore((state) => state.wishList);
   const getWishList = useWishListStore((state) => state.getWishList);
-  console.log(wishList);
 
   useEffect(() => {
-    console.log('fetching wishlist');
     getWishList(1);
   }, []);
+
   return (
     <div className="bg-[url('../public/images/wishlist/background.png')] bg-no-repeat bg-cover py-10 h-screen px-5 pr-8">
       <Navbar className="mb-8" />
       <FilterBar />
-      <div className="grid grid-cols-2 gap-6.5 overflow-y-scroll yellowScroll overflow-x-hidden h-[80%] pr-5">
-        {WISHLIST_DATA.map((product: WishlistTypes) => {
+      <div className="grid grid-cols-2 gap-6.5 overflow-y-scroll yellowScroll overflow-x-hidden pr-5 max-w-[1680px] mx-auto">
+        {wishList.map((product: WatchList) => {
           return <WishListProdCard key={product.id} product={product} />;
         })}
       </div>
