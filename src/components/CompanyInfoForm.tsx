41975b8a42db68cc0199f78e90ed81b0c885cec0
Shawn <shawn.tseng@pivoterra.co>
2023-09-07 15:23:45 +0500

    registration post api added

--- src/components/CompanyInfoForm.tsx
+++ src/components/CompanyInfoForm.tsx
@@ -1,29 +1,116 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import classNames from 'classnames';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
+import { useCompanyStore } from '@/store/company';
 import { InputSize } from '@/type';
 import { CompanyRegistrationSteps } from '@/util/constants';
 
-import CompanyDocumentUpload from './CompanyDocumentUpload';
+import CompanyInputField from './CompanyInputField';
 import CustomButton from './CustomButton';
-import LabelInput from './LabelInput';
+import UploadDocuments from './UploadDocuments';
 
 interface IProps {
   nextStep: (val: number) => void;
 }
 
+export type FormValues = {
+  name: string;
+  registration_number: string;
+  capital: number;
+  phone: string;
+  founding_date: string;
+  representative: string;
+  address: {
+    additionalProp1: string;
+    additionalProp2: string;
+    additionalProp3: string;
+    additionalProp4: string;
+    additionalProp5: string;
+    additionalProp6: string;
+    additionalProp7: string;
+    additionalProp8: string;
+    additionalProp9: string;
+    additionalProp10: string;
+    additionalProp11: string;
+    additionalProp12: string;
+  };
+  contact_address: string;
+  registration_document: string;
+};
+
+const schema = yup
+  .object({
+    name: yup.string().required('Name is required'),
+    registration_number: yup.string().required('registration_number is required'),
+    capital: yup.number().required('Capital is required'),
+    phone: yup.string().required('Phone is required'),
+    founding_date: yup.string().required('Founding date is required'),
+    representative: yup.string().required('Representative is required'),
+    contact_address: yup.string().required('Contact address is required'),
+    //  address type
+
+    address: yup
+      .object()
+      .shape({
+        additionalProp1: yup.string().required('Address is required'),
+        additionalProp2: yup.string().required('Address is required'),
+        additionalProp3: yup.string().required('Address is required'),
+        additionalProp4: yup.string().required('Address is required'),
+        additionalProp5: yup.string().required('Address is required'),
+        additionalProp6: yup.string().required('Address is required'),
+        additionalProp7: yup.string().required('Address is required'),
+        additionalProp8: yup.string().required('Address is required'),
+        additionalProp9: yup.string().required('Address is required'),
+        additionalProp10: yup.string().required('Address is required'),
+        additionalProp11: yup.string().required('Address is required'),
+        additionalProp12: yup.string().required('Address is required')
+      })
+      .required(),
+    registration_document: yup.string().required('Registration document is required')
+  })
+  .required();
+
 const CompanyInfoForm = ({ nextStep }: IProps) => {
+  const [isChecked, setIsChecked] = useState(false);
+
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm();
+  } = useForm<FormValues>({ resolver: yupResolver(schema) });
+
+  const createCompany = useCompanyStore((state) => state.createCompany);
+
+  const onSubmit = handleSubmit(async (data) => {
+    const concatenatedAddresss = `${data.address.additionalProp1}, ${data.address.additionalProp2}, ${data.address.additionalProp3}, ${data.address.additionalProp4}, ${data.address.additionalProp5}, ${data.address.additionalProp6}, ${data.address.additionalProp7}, ${data.address.additionalProp8} ${data.address.additionalProp9}, ${data.address.additionalProp10}, ${data.address.additionalProp11}, ${data.address.additionalProp12}`;
+    await createCompany({
+      id: 0,
+      name: data.name,
+      registration_number: data.registration_number,
+      phone: data.phone,
+      representative: data.representative,
+      capital: Number(data.capital),
+      founding_date: data.founding_date,
+      contact_address: isChecked ? concatenatedAddresss : data.contact_address,
+      address: {
+        additionalProp1: `${data.address.additionalProp1}, ${data.address.additionalProp2}, ${data.address.additionalProp3}, ${data.address.additionalProp4}`,
+        additionalProp2: `${data.address.additionalProp5}, ${data.address.additionalProp6}, ${data.address.additionalProp7}, ${data.address.additionalProp8}`,
+        additionalProp3: `${data.address.additionalProp9}, ${data.address.additionalProp10}, ${data.address.additionalProp11}, ${data.address.additionalProp12}`
+      },
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+      registration_document:
+        'https://st2.depositphotos.com/1104517/11967/v/950/depositphotos_119675554-stock-illustration-male-avatar-profile-picture-vector.jpg'
+    });
+
+    nextStep(CompanyRegistrationSteps.REPRESENTATIVE_INFO_FORM);
+  });
+
   return (
-    <form
-      onSubmit={handleSubmit(() => {
-        nextStep(CompanyRegistrationSteps.REPRESENTATIVE_INFO_FORM);
-      })}
-    >
+    <form onSubmit={onSubmit}>
       <div className="w-max mx-auto">
         <div className="flex flex-row mb-7">
           <h1 className="text-navy-blue text-2.5xl flex flex-row">
@@ -33,8 +120,8 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
         </div>
         <div className="flex flex-row gap-28 justify-center w-full">
           <div>
-            <LabelInput
-              id="company_name"
+            <CompanyInputField
+              id="name"
               isRequired={true}
               type="text"
               register={register}
@@ -44,8 +131,8 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
               size={InputSize.SMALL}
             />
 
-            <LabelInput
-              id="member_phone"
+            <CompanyInputField
+              id="registration_number"
               isRequired={true}
               type="text"
               register={register}
@@ -56,8 +143,8 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
               size={InputSize.SMALL}
             />
 
-            <LabelInput
-              id="representative_chinese_name"
+            <CompanyInputField
+              id="representative"
               isRequired={true}
               type="text"
               register={register}
@@ -67,8 +154,8 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
               size={InputSize.SMALL}
             />
 
-            <LabelInput
-              id="paid_in_capital"
+            <CompanyInputField
+              id="capital"
               isRequired={true}
               type="text"
               register={register}
@@ -78,8 +165,8 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
               size={InputSize.SMALL}
             />
 
-            <LabelInput
-              id="approved_establishment_date"
+            <CompanyInputField
+              id="founding_date"
               isRequired={true}
               type="date"
               placeholder='"YYYY-MM-DD"'
@@ -90,60 +177,97 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
               size={InputSize.SMALL}
             />
             <div className="flex gap-2.7">
-              <label className="text-black text-right font-semibold w-[128px] mb-5.2">會員聯絡地址 :</label>
+              <label className="text-black text-right font-semibold w-[128px] mb-5.2">公司登記地址 :</label>
               <div className="flex flex-col">
                 <div className="absolute flex flex-col -translate-y-1.5">
                   <div className="flex flex-row my-1">
                     <select
+                      id="address.additionalProp1"
+                      {...register(`address.additionalProp1`, { required: true })}
                       className={classNames(
                         'min-[1700px]:w-23.2 min-[1550px]:w-20 w-19 min-[1550px]:text-mdbase min-[1200px]:text-xms text-xxs',
                         Style
                       )}
+                      defaultValue="縣市"
                     >
-                      <option className="text-black">縣市</option>
-                      <option className="text-black">縣市</option>
-                      <option className="text-black">縣市</option>
+                      <option value="縣市" className="text-black">
+                        縣市
+                      </option>
+                      <option value="縣市" className="text-black">
+                        縣市
+                      </option>
+                      <option value="縣市" className="text-black">
+                        縣市
+                      </option>
                     </select>
                     <select
+                      id="address.additionalProp2"
+                      {...register(`address.additionalProp2`, { required: true })}
                       className={classNames(
                         'min-[1700px]:w-23.2 min-[1550px]:w-20 w-19 min-[1550px]:text-mdbase min-[1200px]:text-xms text-xxs',
                         Style
                       )}
                     >
-                      <option className="text-black">鄉鎮市區</option>
-                      <option className="text-black">鄉鎮市區</option>
-                      <option className="text-black">鄉鎮市區</option>
+                      <option value="鄉鎮市區" className="text-black">
+                        鄉鎮市區
+                      </option>
+                      <option value="鄉鎮市區" className="text-black">
+                        鄉鎮市區
+                      </option>
+                      <option value="鄉鎮市區" className="text-black">
+                        鄉鎮市區
+                      </option>
                     </select>
                     <input
+                      id="address.additionalProp3"
+                      {...register(`address.additionalProp3`, { required: true })}
                       type="text"
                       className={classNames(
                         'min-[1700px]:w-16 min-[1550px]:w-14 min-[1200px]:w-13 w-12 mr-2 px-5',
-                        Style
+                        Style,
+                        {
+                          'border-bright-red border': errors.address?.additionalProp3
+                        }
                       )}
                     />
                     <input
+                      id="address.additionalProp4"
+                      {...register(`address.additionalProp4`, { required: true })}
                       type="text"
                       placeholder="路、街、村、段"
-                      {...register('local_address')}
                       className={classNames('px-5 min-[1700px]:w-36 min-[1550px]:w-33 min-[1200px]:w-31 w-29', Style, {
-                        'border-bright-red border': errors.local_address
+                        'border-bright-red border': errors.address?.additionalProp4
                       })}
                     />
                   </div>
                   <div className="flex flex-row my-1 items-center">
-                    {address_row_1.map((item) => {
+                    {address_row_1.map((item: string, idx: number) => {
+                      let id = '' as
+                        | 'address.additionalProp5'
+                        | 'address.additionalProp6'
+                        | 'address.additionalProp7'
+                        | 'address.additionalProp8';
+
+                      if (idx === 0) id = 'address.additionalProp5';
+                      else if (idx === 1) id = 'address.additionalProp6';
+                      else if (idx === 2) id = 'address.additionalProp7';
+                      else id = 'address.additionalProp8';
+
                       return (
                         <>
                           <input
+                            id={id}
+                            {...register(id, { required: false })}
                             type="text"
                             className={classNames(
                               'min-[1700px]:w-15 min-[1550px]:w-13 min-[1200px]:w-10 w-9 mr-1.5 min-[1400px]:px-5 px-4 text-center',
                               Style,
                               {
-                                'border-bright-red border': errors[item]
+                                'border-bright-red border':
+                                  errors.address &&
+                                  Object.prototype.hasOwnProperty.call(errors.address, id.replace('address.', ''))
                               }
                             )}
-                            {...register(item, { required: true })}
                           />
                           <label className="text-black font-bold mr-1.5 text-[12px]">{item}</label>
                         </>
@@ -151,33 +275,48 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
                     })}
                   </div>
                   <div className="flex flex-row my-1 items-center">
-                    {address_row_2.map((item) => {
+                    {address_row_2.map((item, idx) => {
+                      let id = '' as
+                        | 'address.additionalProp9'
+                        | 'address.additionalProp10'
+                        | 'address.additionalProp11'
+                        | 'address.additionalProp12';
+
+                      if (idx === 0) id = 'address.additionalProp9';
+                      else if (idx === 1) id = 'address.additionalProp10';
+                      else if (idx === 2) id = 'address.additionalProp11';
+                      else id = 'address.additionalProp12';
+
                       return (
                         <>
                           <input
+                            id={id}
                             type="text"
                             className={classNames(
                               'min-[1700px]:w-15 min-[1550px]:w-13 min-[1200px]:w-10 w-9 mr-1.5 min-[1400px]:px-5 px-4 text-center',
                               Style,
                               {
-                                'border-bright-red border': errors[item]
+                                'border-bright-red border':
+                                  errors.address &&
+                                  Object.prototype.hasOwnProperty.call(errors.address, id.replace('address.', ''))
                               }
                             )}
-                            {...register(item, { required: true })}
+                            {...register(id, { required: true })}
                           />
                           <label className="text-black font-bold mr-1.5 text-[12px]">{item}</label>
                         </>
                       );
                     })}
                   </div>
+                  {errors.address && <p className="text-[#FF0000] text-xs font-normal">( 紅色框格請務必填寫 )</p>}
                 </div>
               </div>
             </div>
           </div>
           <div>
             <div>
-              <LabelInput
-                id="member_phone"
+              <CompanyInputField
+                id="phone"
                 isRequired={true}
                 type="text"
                 register={register}
@@ -188,11 +327,16 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
               />
               <div>
                 <div className="flex flex-row items-center mb-1 ml-[155px] font-bold">
-                  <input type="checkbox" />
+                  <input
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={() => setIsChecked(!isChecked)}
+                    className="w-4 h-4"
+                  />
                   <label className="text-black ml-2 text-xs">同公司登記地址</label>
                 </div>
-                <LabelInput
-                  id="member_contact_address"
+                <CompanyInputField
+                  id="contact_address"
                   isRequired={false}
                   type="text"
                   register={register}
@@ -204,13 +348,17 @@ const CompanyInfoForm = ({ nextStep }: IProps) => {
                 <label className="text-black text-right font-semibold col-span-1 mb-5.2 w-[128px]">
                   營業登記文件 :
                 </label>
-                <CompanyDocumentUpload register={register} />
+                <UploadDocuments register={register} />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <CustomButton className="text-white bg-navy-blue px-4.5 py-0.7 font-bold rounded-md absolute bottom-7 right-5">
+      <CustomButton
+        type="submit"
+        onClick={() => console.log('errors', errors)}
+        className="text-white bg-navy-blue px-4.5 py-0.7 font-bold rounded-md absolute bottom-7 right-5"
+      >
         储存 | 下一步
       </CustomButton>
     </form>
@@ -222,4 +370,4 @@ export default CompanyInfoForm;
 const Style =
   'rounded-full text-black shadow-company-registration-input bg-white min-[1550px]:h-9.5 min-[1200px]:h-7.5 h-6 min-[1550px]:px-2 min-[1200px]:px-1.5 px-1 py-2.5 text-black min-[1550px]:text-mdbase min-[1200px]:text-xms text-xxs outline-none';
 const address_row_1 = ['鄰', '巷', '弄', '街'];
-const address_row_2 = ['號之', ',', '號之', '室'];
+const address_row_2 = ['號之', ',', '樓之', '室'];
---
+++ src/components/UploadDocuments.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { UseFormRegister } from 'react-hook-form';
+
+import { FormValues } from './CompanyInfoForm';
+
+const UploadDocuments = ({ register }: { register: UseFormRegister<FormValues> }) => {
+  const [documentQuantity, setDocumentQuantity] = useState(1);
+  return (
+    <div className="flex flex-col max-w-[372px] max">
+      <p className="text-black min-[1500px]:text-base text-sm">
+        影業登記文件檔上傳,限小於<span className="text-bright-red">2MB</span>的JPG、PNG檔案。
+      </p>
+      <p className="text-navy-blue underline">了解營業登記文件上傳...</p>
+      <div className="flex flex-row flex-wrap max-w-[360px] gap-4 mt-2">
+        {Array.from({ length: documentQuantity }, (value, index) => index + 1).map((item) => {
+          return (
+            <div className="flex flex-col rounded-xl border items-center border-silverstone h-23.2 w-26.7">
+              <p className="text-[36px] text-black mt-2">{item}</p>
+              <label className="rounded-full bg-navy-blue p-0.5 px-5 text-xms text-white" htmlFor="get-file">
+                選擇
+              </label>
+              <input id="get-file" {...register('registration_document')} type="file" className="invisible" />
+            </div>
+          );
+        })}
+        <div
+          className="flex flex-col rounded-xl border border-silverstone bg-light-trans-grey items-center justify-center h-23.2 w-26.7"
+          onClick={() => setDocumentQuantity((prev) => prev + 1)}
+        >
+          <img src="/images/operation-record/plus-icon.svg" alt="add new doc" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UploadDocuments;
