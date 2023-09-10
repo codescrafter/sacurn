e7a951c1af794f82a5b397954e427924cb0d2486
Shawnz <53845962+shawn1126@users.noreply.github.com>
2023-09-11 02:44:19 +0800

    Update RepresentativeInfoForm.tsx
    Add dropdowns for chinese text

--- src/components/RepresentativeInfoForm.tsx
+++ src/components/RepresentativeInfoForm.tsx
@@ -1,6 +1,6 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import classNames from 'classnames';
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
 import { FieldErrors, FieldValues, useForm, UseFormRegister } from 'react-hook-form';
 import * as yup from 'yup';
 
@@ -35,6 +35,7 @@ const schema = yup
 
 const RepresentativeInfoForm = ({ nextStep }: IProps) => {
   const [uploadedDocs, setUploadedDocs] = useState<File[]>([]);
+  const [selectedValue, setSelectedValue] = useState<string>('');
 
   const companyId = useUserStore.getState().companyId;
   const {
@@ -64,6 +65,11 @@ const RepresentativeInfoForm = ({ nextStep }: IProps) => {
     }
   });
 
+  // Attach onChange event handlers to the radio buttons
+  const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(event.target.value);
+  };
+
   return (
     <form onSubmit={onSubmit}>
       <div className="mx-auto px-5 w-[570px]">
@@ -84,6 +90,7 @@ const RepresentativeInfoForm = ({ nextStep }: IProps) => {
                     {...register('representative_country')}
                     value="本國籍"
                     className="border-navy-blue w-3.2 checked:bg-navy-blue"
+                    onChange={handleRadioChange}
                   />
                   <label className="text-xs font-bold">本國籍</label>
                 </div>
@@ -93,75 +100,74 @@ const RepresentativeInfoForm = ({ nextStep }: IProps) => {
                     {...register('representative_country')}
                     value="外國籍"
                     className="border-navy-blue w-3.2 checked:bg-navy-blue"
+                    onChange={handleRadioChange}
                   />
                   <label className="text-xs font-bold">外國籍</label>
                 </div>
-                <div className="flex gap-0.5 items-center">
-                  <input
-                    type="radio"
-                    {...register('representative_country')}
-                    value="法人"
-                    className="border-navy-blue w-3.2 checked:bg-navy-blue"
-                  />
-                  <label className="text-xs font-bold">法人</label>
-                </div>
               </div>
-              <SimpleSelect
+
+              {/* <SimpleSelect
                 register={register}
                 id="representative_id_card_number"
                 className="w-[286px]"
-                options={['國民身分證', '國民身分證', '國民身分證']}
-              />
+                options={selectedValue === '本國籍' ? ['國民身分證'] : ['護照']}
+              /> */}
             </div>
           </div>
+
           <LabelInput
             register={register}
             type="text"
             id="representative_id_card_number"
             isRequired={true}
-            heading="代表人身分證字號"
-            placeholder="請輸入代表人身分證字號"
+            heading={selectedValue === '本國籍' ? '代表人身分證字號' : '代表人護照號碼'}
+            placeholder={selectedValue === '本國籍' ? '請輸入代表人身分證字號' : '請輸入代表人護照號碼'}
             errors={errors}
             errorMessage="必填字段"
           />
+
           <div className="flex gap-2.7 mb-5.5 items-center">
-            <p className="text-base text-black w-[144px] text-right">身分證發證日期: </p>
+            <p className="text-base text-black w-[144px] text-right">
+              {selectedValue === '本國籍' ? '身分證發證日期' : '發照日期:'}
+            </p>
             <div className="flex justify-between w-[286px] items-center">
               <SimpleSelect
                 register={register}
                 id="representative_id_card_issue_date"
                 className="w-21.7"
-                options={['年']}
+                options={yearArray.map((year) => year.toString())}
               />
               /
               <SimpleSelect
                 register={register}
                 id="representative_id_card_issue_date"
                 className="w-21.7"
-                options={['年']}
+                options={months.map((month) => month)}
               />
               /
               <SimpleSelect
                 register={register}
                 id="representative_id_card_issue_date"
                 className="w-21.7"
-                options={['年']}
+                options={dates.map((date) => date)}
               />
             </div>
           </div>
           <div className="flex gap-2.7 mb-5.5 items-center">
-            <p className="text-base text-black w-[144px] text-right">身分證發證地點: </p>
+            <p className="text-base text-black w-[144px] text-right">
+              {selectedValue === '本國籍' ? '身分證發證地點:' : '發照機關:'}
+            </p>
             <SimpleSelect
               register={register}
               id="representative_id_card_issue_location"
               className="w-[87px]"
-              options={['選擇', '選擇']}
+              options={regionCityLocation.map((location) => location)}
             />
             <SimpleSelect
               register={register}
               id="representative_id_card_issue_location"
               className="w-[81px]"
-              options={['初發', '初發']}
+              options={['初發', '補發', '換發']}
             />
           </div>
           <LabelInput
@@ -169,13 +175,15 @@ const RepresentativeInfoForm = ({ nextStep }: IProps) => {
             type="text"
             id="representative_birthday"
             isRequired={true}
-            heading="出生年月日"
+            heading={selectedValue === '本國籍' ? '出生年月日:' : '出生日期:'}
             placeholder="YYYY-MM-DD"
             errors={errors}
             errorMessage="必填字段"
           />
           <div className="flex gap-2.7">
-            <p className="text-black text-base min-w-[144px] text-right">身分證文件:</p>
+            <p className="text-black text-base min-w-[144px] text-right">
+              {selectedValue === '本國籍' ? '身分證文件上傳:' : '護照文件上傳:'}
+            </p>
             {/* <UploadDocuments register={register} /> */}
             <UploadDocuments uploadedDocs={uploadedDocs} setUploadedDocs={setUploadedDocs} />
           </div>
@@ -302,3 +310,63 @@ const LabelInput = ({
     </div>
   );
 };
+const regionCityLocation = [
+  '台北市',
+  '新北市',
+  '基隆市',
+  '新竹市',
+  '桃園市',
+  '新竹縣',
+  '宜蘭縣',
+  '臺中市',
+  '苗栗縣',
+  '彰化縣',
+  '南投縣',
+  '雲林縣',
+  '高雄市',
+  '臺南市',
+  '嘉義市',
+  '嘉義縣',
+  '屏東縣',
+  '澎湖縣',
+  '花蓮縣',
+  '臺東縣',
+  '金門縣',
+  '連江縣'
+];
+const months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+const dates = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+  '24',
+  '25',
+  '26',
+  '27',
+  '28',
+  '29',
+  '30',
+  '31'
+];
+const yearArray = Array.from({ length: 124 }, (_, index) => 1900 + index);
+
