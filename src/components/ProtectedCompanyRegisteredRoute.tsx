289662dec63f4260cfe3d146bd06db17c10c23c7
eric.lin <seekingfor520@gmail.com>
2023-09-13 02:43:44 +0800

    Fixed company router protect

--- src/components/ProtectedCompanyRegisteredRoute.tsx
+++ src/components/ProtectedCompanyRegisteredRoute.tsx
@@ -1,5 +1,5 @@
 import cookies from 'js-cookie';
-import { Fragment, ReactNode } from 'react';
+import { Fragment, ReactNode, useRef, useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 import { useCompanyStore } from '@/store/company';
@@ -15,6 +15,8 @@ export const ProtectedCompanyRegisteredRoute = ({ children }: ProtectedCompanyRe
   const company = useCompanyStore((state) => state.company);
   const getCompany = useCompanyStore((state) => state.getCompany);
   const navigate = useNavigate();
+  const [isValidFinished, setInValidFinished] = useState(false);
+  const isUpdatingCompany = useRef<boolean>(false);
 
   if (!authResultString) {
     // user is not authenticated
@@ -23,16 +25,19 @@ export const ProtectedCompanyRegisteredRoute = ({ children }: ProtectedCompanyRe
 
   const authResult = JSON.parse(authResultString) as AuthResult;
 
-  const hasCompanyStatus = !(Object.entries(company).length === 0 || company.status === undefined);
+  const hasCompanyStatus = company && company.status && company.status in CompanyStatus;
 
-  if (!hasCompanyStatus && authResult.companyId) {
+  if (!hasCompanyStatus && authResult.companyId && !isUpdatingCompany.current) {
+    isUpdatingCompany.current = true;
     getCompany(authResult.companyId).then((company) => {
-      console.log(company);
       if (company?.status !== CompanyStatus.PassReview) {
         navigate('/company-registration');
       }
+      setInValidFinished(true);
     });
   }
 
-  return <Fragment>{children}</Fragment>;
+  if (isValidFinished) {
+    return <Fragment>{children}</Fragment>;
+  }
 };
