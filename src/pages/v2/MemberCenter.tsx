a07b4e9d8b188c3be0ab4be3b9caca5c002a8f96
khadjafatima409 <khadijafatima530@gmail.com>
2023-09-30 14:36:11 -0700

    membercenter-data not submitted

--- src/pages/v2/MemberCenter.tsx
+++ src/pages/v2/MemberCenter.tsx
@@ -1,22 +1,84 @@
+import { DevTool } from '@hookform/devtools';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+// import { useForm } from 'react-hook-form';
+// import { FormValues } from '@/components/CompanyInfoForm';
 import Layout from '@/components/v2/Layout';
 
 import MemberCenterInput from './MemberCenterInput';
-
+export type FormValues = {
+  name: string;
+  professionaltitle: string;
+  email: string;
+  phone: string;
+  extention: string;
+};
+const schema = yup.object({
+  name: yup.string().required('name is requred'),
+  professionaltitle: yup.string().required('rquired'),
+  email: yup.string().email('Email format is not valid').required('email is requred'),
+  phone: yup.string().required('例: 0x-000111或09xx-000111'),
+  extention: yup.string().required('extention name is not correct ')
+});
 const MemberCenter = () => {
+  const form = useForm<FormValues>({
+    defaultValues: {
+      name: '',
+      professionaltitle: '',
+      email: '',
+      phone: '',
+      extention: ''
+    },
+    resolver: yupResolver(schema)
+  });
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+  const onSubmit = (data: FormValues) => {
+    // eslint-disable-next-line no-debugger
+    // debugger;
+    console.log('form is submited', data);
+  };
+  console.log(errors);
   return (
     <Layout>
-      <form action="">
-        <div className=" bg-[#FFFFFF] bg-opacity-80 rounded-lg border p-7 m-auto my-28  w-[90%]">
+      <form action="" onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className=" bg-white bg-opacity-80 rounded-lg border p-7 m-auto my-28  w-[90%]">
           <div className="flex gap-5 ">
             {/* child 1 */}
             <div className=" flex flex-col gap-17 items-center">
               <h1 className="text-lg font-bold text-deepseablue">| 新增使用者</h1>
-              <div className="bg-deepseablue h-28 w-28 rounded-full flex justify-center items-center">
-                <h2 className="text-[#FFFFFF] "> 更換照片</h2>
+              <div
+                className="bg-deepseablue h-28 w-28 rounded-full flex justify-center items-center"
+                // onClick={() => {
+                //   console.log(errors);
+                // }}
+              >
+                <h2 className="text-white "> 更換照片</h2>
               </div>
             </div>
             {/* second child */}
-            <MemberCenterInput />
+            <div className="flex flex-col gap-4 pt-13 pb-10 ">
+              <MemberCenterInput id="name" heading="姓名" type="text" isRequired={true} register={register} />
+              <MemberCenterInput
+                id="professionaltitle"
+                heading="職稱"
+                type="text"
+                isRequired={true}
+                register={register}
+              />
+              <MemberCenterInput id="email" heading="Email" type="email" isRequired={true} register={register} />
+              <MemberCenterInput
+                id="phone"
+                heading="電話"
+                type="number"
+                register={register}
+                errorMessage="例: 0x-000111或09xx-000111"
+                isRequired={true}
+              />
+              <MemberCenterInput id="extention" heading="分機" type="text" isRequired={true} register={register} />
+            </div>
             {/* Third Child */}
             <div className=" flex flex-col justify-between ">
               {/* third-child-1 */}
@@ -24,14 +86,14 @@ const MemberCenter = () => {
                 <label htmlFor="" className="font-bold text-xl text-deepseablue">
                   操作權限
                 </label>
-                {/* <input type="select" name="" id="" className="rounded-full h-10 w-60 bg-white outline-none px-4" /> */}
-                <div className="w-60 h-10 rounded-full bg-white py-2 pl-4">
+                <div className="w-60 h-10 rounded-full bg-white pl-4">
                   <select name="" id="">
                     <option value="">身份選擇</option>
                     <option value="">Saab</option>
                     <option value="">Mercedes</option>
                     <option value="">Audi</option>
                   </select>
+                  <input type="" name="" id="" className="rounded-full h-10 w-[132px] bg-white outline-none px-4 " />
                 </div>
               </div>
 
@@ -47,8 +109,16 @@ const MemberCenter = () => {
                 </div>
                 {/* third-child-2 */}
                 <div className="flex justify-end gap-6 items-center">
-                  <button className="border px-10 py-1 font-bold rounded-xl outline-none text-deepseablue">取消</button>
-                  <button className="px-10 py-1 font-bold rounded-xl bg-deepseablue text-white outline-none ">
+                  <button
+                    type="button"
+                    className="border px-10 py-1 font-bold rounded-xl outline-none text-deepseablue"
+                  >
+                    取消
+                  </button>
+                  <button
+                    type="button"
+                    className="px-10 py-1 font-bold rounded-xl bg-deepseablue text-white outline-none "
+                  >
                     確認
                   </button>
                 </div>
@@ -57,6 +127,7 @@ const MemberCenter = () => {
           </div>
         </div>
       </form>
+      <DevTool control={control} />
     </Layout>
   );
 };
--- src/pages/v2/MemberCenterInput.tsx
+++ src/pages/v2/MemberCenterInput.tsx
@@ -1,39 +1,33 @@
-const MemberCenterInput = () => {
+// import { error } from 'console';
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+
+import { FormValues } from './MemberCenter';
+interface IProps {
+  id: string;
+  heading: string;
+  type: string;
+  isRequired: boolean;
+  register: UseFormRegister<FormValues>;
+  errors?: FieldErrors<FieldValues>;
+  errorMessage?: string;
+}
+const MemberCenterInput = ({ id, heading, type, register, isRequired, errorMessage, errors }: IProps) => {
   return (
-    <div className=" flex flex-col gap-4 pt-13 pb-10">
-      <div className="flex  justify-between items-center gap-2 ">
-        <label htmlFor="" className="text-xl font-bold text-deepseablue">
-          姓名
+    <div className="flex flex-col ">
+      <div className="flex  justify-between items-center w-72 gap-3">
+        <label htmlFor="name" className="text-xl font-bold text-deepseablue">
+          {heading}
         </label>
-        <input type="text" name="" id="" className="rounded-full h-10 w-60 bg-white outline-none px-4" />
+        <input
+          type="text"
+          className="rounded-full h-10 w-56 bg-white outline-none px-4"
+          {...register(id as 'name' | 'phone' | 'professionaltitle' | 'email' | 'phone' | 'extention', {
+            required: isRequired
+          })}
+          typeof={type}
+        />
       </div>
-      <div className="flex justify-between items-center gap-2">
-        <label htmlFor="" className="text-xl font-bold text-deepseablue ">
-          職稱
-        </label>
-        <input type="text" name="" id="" className="rounded-full h-10 w-60 outline-none px-4" />
-      </div>
-      <div className="flex justify-between items-center gap-2">
-        <label htmlFor="" className="text-xl font-bold text-deepseablue">
-          Email
-        </label>
-        <input type="text" name="" id="" className="rounded-full h-10 w-60 outline-none px-4" />
-      </div>
-      <div>
-        <div className="flex justify-between items-center gap-2">
-          <label htmlFor="" className="text-xl font-bold text-deepseablue">
-            電話
-          </label>
-          <input type="text" name="" id="" className="rounded-full h-10 w-60 outline-none px-4 " />
-        </div>
-        <p className="text-xs text-sunset-orange ml-19 mt-1">例: 0x-000111或09xx-000111</p>
-      </div>
-      <div className="flex items-center gap-6 -mt-3">
-        <label htmlFor="" className="text-xl font-bold text-deepseablue">
-          分機
-        </label>
-        <input type="text" name="" id="" className="rounded-full h-10 w-31 outline-none px-4 " />
-      </div>
+      {errors && errors[id] && <p className="text-xs text-sunset-orange ml-19 mt-1">{errorMessage}</p>}
     </div>
   );
 };
