481770638283bd71e126aff48736e6d5d08e19eb
khadjafatima409 <khadijafatima530@gmail.com>
2023-10-06 00:49:29 -0700

    Updated Cards

--- src/pages/v2/PlatformInstructionCard.tsx
+++ src/pages/v2/PlatformInstructionCard.tsx
@@ -1,6 +1,12 @@
 import AddIcon from '@mui/icons-material/Add';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import RemoveIcon from '@mui/icons-material/Remove';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
 import { useState } from 'react';
+// import * as React from 'react';
 interface IProps {
   Card_heading: string;
   Card_name: string;
@@ -9,14 +15,21 @@ interface IProps {
 }
 
 const PlatformInstructionCard = ({ Card_heading, Card_name, Card_description, Card_heading2 }: IProps) => {
-  const [show, setShow] = useState(true);
-  const [show2, setShow2] = useState(true);
+  // const [show, setShow] = useState(false);
+  // const [show2, setShow2] = useState(false);
+  const [showAccordion, setShowAccordion] = useState(false);
+  const [showAccordion2, setShowAccordion2] = useState(false);
+
+  // const toggleAccordion = () => {
+  //   setShowAccordion(!showAccordion);
+  // };
+
   return (
     <div className="">
       <h1 className="text-3xl text-DeepSeaBlue font-bold max-[1400px]:text-xl">{Card_heading}</h1>
 
       <div className="">
-        <div className="flex justify-between px-8 text-xl font-bold py-2  max-[1400px]:text-base">
+        {/* <div className="flex justify-between px-8 text-xl font-bold py-2  max-[1400px]:text-base">
           <p>{Card_name}</p>
 
           <button onClick={() => setShow(!show)}>
@@ -35,8 +48,38 @@ const PlatformInstructionCard = ({ Card_heading, Card_name, Card_description, Ca
           <div className={`px-10 h-[100px] overflow-y-scroll scroll-right  ${show ? '' : 'hidden'}`}>
             {show && <p className="text-xl text-left font-normal py-4  max-[1400px]:text-base"> {Card_description}</p>}
           </div>
-        </div>
-        <div className="flex justify-between px-8 text-xl font-bold py-2">
+        </div> */}
+        <Accordion className="mt-5 ">
+          <AccordionSummary
+            expandIcon={
+              showAccordion ? (
+                <span>
+                  <RemoveIcon onClick={() => setShowAccordion(!showAccordion)} />
+                </span>
+              ) : (
+                <span>
+                  <AddIcon onClick={() => setShowAccordion(!showAccordion)} />
+                </span>
+              )
+            }
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className=" ">
+              <p className="  px-4 text-xl font-bold max-[1400px]:text-base">{Card_name}</p>
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            className={`pl-10 h-[100px] overflow-y-scroll scroll-right w-[100%] border-b-2 border-grey-ghoose border-dashed bg-Geyser ${
+              showAccordion ? '' : 'hidden'
+            }`}
+          >
+            <Typography className="  ">
+              <p className="px-4 text-xl text-left font-normal max-[1400px]:text-base"> {Card_description}</p>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        {/* <div className="flex justify-between px-8 text-xl font-bold py-2">
           <p>{Card_heading2}</p>
           <button onClick={() => setShow2(!show2)}>
             {show2 ? (
@@ -56,7 +99,37 @@ const PlatformInstructionCard = ({ Card_heading, Card_name, Card_description, Ca
               <p className="text-xl text-left font-normal py-4  max-[1400px]:text-base "> {Card_description}</p>
             )}
           </div>
-        </div>
+        </div> */}
+        <Accordion className="mt-3 mb-5">
+          <AccordionSummary
+            expandIcon={
+              showAccordion2 ? (
+                <span>
+                  <RemoveIcon onClick={() => setShowAccordion2(!showAccordion2)} />
+                </span>
+              ) : (
+                <span>
+                  <AddIcon onClick={() => setShowAccordion2(!showAccordion2)} />
+                </span>
+              )
+            }
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>
+              <p className="px-4 text-xl font-bold max-[1400px]:text-base">{Card_heading2}</p>
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            className={` px-10 h-[100px] overflow-y-scroll scroll-right w-[100%] border-b-2 border-grey-ghoose border-dashed bg-Geyser  ${
+              showAccordion2 ? '' : 'hidden'
+            }`}
+          >
+            <Typography className="text-xl text-left font-normal py-4  max-[1400px]:text-base">
+              {Card_description}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </div>
     </div>
   );
--- src/pages/v2/PlatformUsageInstructions.tsx
+++ src/pages/v2/PlatformUsageInstructions.tsx
@@ -3,7 +3,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import PlatformInstructionCard from './PlatformInstructionCard';
 const PlatformUsageInstructions = () => {
   return (
-    <div className="w-[90%] h-[600px]rounded-lg bg-trans-white mt-4 p-2 pt-4 ml-15 min-[1400px]:mt-[40px] min-[1700px]:mt[150px]">
+    <div className="w-[90%] h-[560px] rounded-lg bg-trans-white mt-4 p-2 pt-4 ml-15 min-[1400px]:mt-[40px]  ">
       <div className="flex justify-end pr-4">
         <div className="w-[40%] border text-grey-ghoose text-xs rounded-full bg-white pl-4 flex justify-between p-2">
           <input type="text" placeholder="輸入想要搜尋的碳權名稱,代號或是關鍵字" className=" w-[60%] outline-none" />
@@ -13,8 +13,8 @@ const PlatformUsageInstructions = () => {
         </div>
       </div>
 
-      <div className="h-[500px] overflow-y-scroll yellowScroll  min-[1400px]:h-[684px]">
-        <div className="w-[100%] p-5">
+      <div className="h-[500px] overflow-y-scroll yellowScroll min-[1700px]:h-[780px] border">
+        <div className="w-[100%] p-5 min-[1400px]:h-[884px] min-[1500]:h-[970px]  ">
           {INSTRUCTIONCARD_DATA.map((element) => {
             return (
               <PlatformInstructionCard
