a17f3f9c8b09f3524d5e7734ad9d0cc2d86bdcd2
aliraza2u <raza8r@gmail.com>
2023-10-07 21:22:09 +0500

    add input fields

--- src/pages/v2/AccountInformation.tsx
+++ src/pages/v2/AccountInformation.tsx
@@ -1,10 +1,11 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import classNames from 'classnames';
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { FieldErrors, UseFormRegister } from 'react-hook-form';
 import * as yup from 'yup';
 
+import CustomButton from '@/components/CustomButton';
 import Layout from '@/components/v2/Layout';
 import { AccountInformationTypes } from '@/type';
 
@@ -14,6 +15,7 @@ export interface AccountInfoValues {
   email: string;
   phone: string;
   password: string;
+  confirmPassword: string;
 }
 
 const Schema = yup.object({
@@ -24,10 +26,16 @@ const Schema = yup.object({
     .string()
     .required('例如：0x-000111 或 09xx-000111')
     .matches(/^09\d{8}$/, '例如：0x-000111 或 09xx-000111'),
-  password: yup.string().required('請輸入密碼')
+  password: yup.string().required('請輸入密碼'),
+  confirmPassword: yup
+    .string()
+    .required('請再次輸入密碼')
+    .oneOf([yup.ref('password')], '密碼不一致')
 });
 
 const AccountInformation = () => {
+  const [isPasswordClicked, setIsPasswordClicked] = useState<boolean>(false);
+
   const {
     register,
     handleSubmit,
@@ -50,12 +58,12 @@ const AccountInformation = () => {
 
   return (
     <Layout>
-      <form onSubmit={onSubmit}>
-        <div className="mt-[124px] w-[90%] rounded-lg min-h-[450px] bg-transparent-blue opacity-[0.9] shadow-account-info-box">
+      <form onSubmit={onSubmit} className="flex justify-end mr-4 xl:block">
+        <div className="mt-[124px] w-[90%] rounded-lg bg-transparent-blue opacity-[0.9] shadow-account-info-box min-h-[490px] 2xl:min-h-[530px] 2.5xl:min-h-[600px]">
           <div className="flex">
-            <div className="w-[45%]">
-              <div className="account-information-clip-path rounded-tl-lg rounded-bl-lg bg-white min-h-[450px]">
-                <div className="p-8">
+            <div className="w-[46%]">
+              <div className="account-information-clip-path rounded-tl-lg rounded-bl-lg bg-white min-h-[490px] xl:min-h-[530px] 2.5xl:min-h-[600px]">
+                <div className="pt-12 pl-8">
                   <div>
                     <div className="w-[90px] h-[90px] rounded-[50%] bg-[url('../public/v2/account-pic.svg')] flex items-end justify-center">
                       {/* <img src="/v2/account-pic.svg" alt="user image" className="w-full h-full object-contain" /> */}
@@ -65,14 +73,14 @@ const AccountInformation = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="mt-10">
+                  <div className="mt-10 2.5xl:mt-14">
                     {ACCOUNT_INFORMATION.map(({ key, value }: AccountInformationTypes) => (
                       <div
                         key={key}
-                        className="flex gap-2 text-navy-blue font-semibold 2xl:font-bold text-base 2xl:text-xl mb-4 2xl:mb-8 first:pr-16 w-full"
+                        className="flex gap-2 text-navy-blue font-semibold 2xl:font-bold text-base 2xl:text-xl 2.5xl:text-[22px] mb-4 2xl:mb-8 2.5xl:mb-10 first:pr-16 w-full"
                       >
-                        <p className="whitespace-nowrap w-[35%] xl:w-[28%]">{key}:</p>
-                        <p className="xl:w-[72%]">{value}</p>
+                        <p className="whitespace-nowrap w-[25%]">{key}:</p>
+                        <p className="pr-4">{value}</p>
                       </div>
                     ))}
                   </div>
@@ -80,11 +88,72 @@ const AccountInformation = () => {
               </div>
             </div>
             <div className="w-[55%] flex flex-col gap-6 items-center pt-10 pr-6">
-              <CustomInput errors={errors} label="聯絡人名稱" id="name" type="text" register={register} />
-              <CustomInput errors={errors} label="聯絡人職稱" id="title" type="text" register={register} />
-              <CustomInput errors={errors} label="聯絡人Email" id="email" type="text" register={register} />
-              <CustomInput errors={errors} label="聯絡人電話" id="phone" type="text" register={register} />
-              <CustomInput errors={errors} label="密碼" id="password" type="text" register={register} />
+              <CustomInput
+                errors={errors}
+                label="聯絡人名稱"
+                id="name"
+                type="text"
+                register={register}
+                defaultValue="Albert"
+              />
+              <CustomInput
+                errors={errors}
+                label="聯絡人職稱"
+                id="title"
+                type="text"
+                register={register}
+                defaultValue="CFO"
+              />
+              <CustomInput
+                errors={errors}
+                label="聯絡人Email"
+                id="email"
+                type="text"
+                register={register}
+                defaultValue="albert@xholding.com"
+              />
+              <CustomInput
+                errors={errors}
+                label="聯絡人電話"
+                id="phone"
+                type="text"
+                register={register}
+                defaultValue="02-1234 5678"
+              />
+              <div className="w-full">
+                <CustomInput
+                  errors={errors}
+                  label="密碼"
+                  id="password"
+                  type="password"
+                  register={register}
+                  defaultValue="************"
+                  callback={() => setIsPasswordClicked(true)}
+                />
+                {isPasswordClicked && (
+                  <Fragment>
+                    <div className="flex justify-end">
+                      <p className="text-xs text-white w-[68%] my-1">
+                        提示：密碼需至少有12字元，請混和使用大小寫字母、數字，使密碼更加安全。
+                      </p>
+                    </div>
+                    <CustomInput
+                      id="confirmPassword"
+                      errors={errors}
+                      label=""
+                      type="text"
+                      register={register}
+                      defaultValue=""
+                      placeholder="確認新密碼"
+                    />
+                    <div className="flex justify-end mt-4">
+                      <CustomButton variant="rounded" className="bg-pale-yellow !text-grey">
+                        確認變更密碼
+                      </CustomButton>
+                    </div>
+                  </Fragment>
+                )}
+              </div>
             </div>
           </div>
         </div>
@@ -96,15 +165,19 @@ const AccountInformation = () => {
 export default AccountInformation;
 
 interface CustomInputIProps {
-  id: 'name' | 'title' | 'email' | 'phone' | 'password';
+  id: 'name' | 'title' | 'email' | 'phone' | 'password' | 'confirmPassword';
   register: UseFormRegister<AccountInfoValues>;
+  defaultValue?: string;
   type: string;
   placeholder?: string;
   label?: string;
   errors: FieldErrors<AccountInfoValues>;
+  callback?: () => void;
 }
 
-const CustomInput = ({ id, register, type, placeholder, label, errors }: CustomInputIProps) => {
+const CustomInput = ({ id, register, defaultValue, type, placeholder, label, errors, callback }: CustomInputIProps) => {
+  const [edit, setEdit] = useState<boolean>(false);
+
   return (
     <div
       className={classNames('flex justify-between w-full gap-7', {
@@ -117,12 +190,23 @@ const CustomInput = ({ id, register, type, placeholder, label, errors }: CustomI
       <div className={classNames('flex flex-col relative w-[76%]', { 'gap-0.5': errors && errors[id] })}>
         <div className="rounded-full px-4 py-1 xl:py-2 2xl:py-3 bg-white flex justify-between">
           <input
-            className="outline-none bg-transparent w-[90%]"
+            defaultValue={defaultValue}
+            className="outline-none bg-transparent w-[90%] text-navy-blue text-lg font-semibold 2xl:text-xl 2xl:font-bold"
             {...register(id)}
             placeholder={placeholder}
             type={type}
+            onFocus={() => {
+              setEdit(true);
+              if (id === 'password' && callback) {
+                callback();
+              }
+            }}
           />
-          <img src="/v2/icon/edit-icon.svg" alt="sacurn" className="h-6 w-6 object-contain" />
+          {edit ? (
+            <img src="/v2/icon/tick-icon.svg" alt="sacurn" className="h-6 w-6 object-contain" />
+          ) : (
+            <img src="/v2/icon/edit-icon.svg" alt="sacurn" className="h-6 w-6 object-contain" />
+          )}
         </div>
         <div className="relative w-full">
           {errors && errors[id] && (
