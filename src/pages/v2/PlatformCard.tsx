7a93f2542560a37376398bc5164b5d7efc33da52
khadjafatima409 <khadijafatima530@gmail.com>
2023-10-06 18:52:41 -0700

    Completed Accordian Styling

--- src/pages/v2/PlatformCard.tsx
+++ src/pages/v2/PlatformCard.tsx
@@ -5,6 +5,7 @@ import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
+import classNames from 'classnames';
 import { useState } from 'react';
 interface IProps {
   title: string;
@@ -14,41 +15,25 @@ interface IProps {
 }
 
 const PlatformCard = ({ title, name, description, subTitle }: IProps) => {
-  // const [show, setShow] = useState(false);
-  // const [show2, setShow2] = useState(false);
   const [showAccordion, setShowAccordion] = useState(false);
   const [showAccordion2, setShowAccordion2] = useState(false);
-
-  // const toggleAccordion = () => {
-  //   setShowAccordion(!showAccordion);
-  // };
 
   return (
     <div className="">
       <h1 className="text-3xl text-DeepSeaBlue font-bold max-[1400px]:text-xl">{title}</h1>
-
       <div>
-        {/* <div className="flex justify-between px-8 text-xl font-bold py-2  max-[1400px]:text-base">
-          <p>{Card_name}</p>
-
-          <button onClick={() => setShow(!show)}>
-            {show ? (
-              <span>
-                <RemoveIcon />
-              </span>
-            ) : (
-              <span>
-                <AddIcon />
-              </span>
-            )}
-          </button>
-        </div>
-        <div className="w-[100%] border-b-2 border-grey-ghoose border-dashed bg-Geyser ">
-          <div className={`px-10 h-[100px] overflow-y-scroll scroll-right  ${show ? '' : 'hidden'}`}>
-            {show && <p className="text-xl text-left font-normal py-4  max-[1400px]:text-base"> {Card_description}</p>}
-          </div>
-        </div> */}
-        <Accordion className="mt-5 ">
+        <Accordion
+          className={classNames('mt-5', {
+            'border-b-2 border-grey-ghoose border-dashed ': !{ showAccordion },
+            'border-b-2 border-grey-ghoose border-dashed': { showAccordion }
+          })}
+          sx={{ backgroundColor: 'inherit', boxShadow: '0px 0px 0px 0px #ffffffe6', Opacity: '0.1' }}
+        >
+          {/* ${
+            showAccordion
+              ? 'border-b-2 border-grey-ghoose border-dashed bg-Geyser'
+              : 'border-b-2 border-grey-ghoose border-dashed bg-Geyser'
+          } */}
           <AccordionSummary
             expandIcon={
               showAccordion ? (
@@ -69,7 +54,7 @@ const PlatformCard = ({ title, name, description, subTitle }: IProps) => {
             </Typography>
           </AccordionSummary>
           <AccordionDetails
-            className={`pl-10 h-[100px] overflow-y-scroll scroll-right w-[100%] border-b-2 border-grey-ghoose border-dashed bg-Geyser ${
+            className={`pl-10 h-[90px] overflow-y-scroll scroll-right w-[100%] bg-Geyser ${
               showAccordion ? '' : 'hidden'
             }`}
           >
@@ -78,28 +63,14 @@ const PlatformCard = ({ title, name, description, subTitle }: IProps) => {
             </Typography>
           </AccordionDetails>
         </Accordion>
-        {/* <div className="flex justify-between px-8 text-xl font-bold py-2">
-          <p>{Card_heading2}</p>
-          <button onClick={() => setShow2(!show2)}>
-            {show2 ? (
-              <span>
-                <RemoveIcon />
-              </span>
-            ) : (
-              <span>
-                <AddIcon />
-              </span>
-            )}
-          </button>
-        </div>
-        <div className=" w-[100%] border-b-2 border-grey-ghoose border-dashed bg-Geyser">
-          <div className={`px-10  h-[100px] overflow-y-scroll scroll-right  ${show2 ? '' : 'hidden'} `}>
-            {show2 && (
-              <p className="text-xl text-left font-normal py-4  max-[1400px]:text-base "> {Card_description}</p>
-            )}
-          </div>
-        </div> */}
-        <Accordion className="mt-3 mb-5">
+
+        <Accordion
+          className={classNames('mt-3 mb-5', {
+            'border-b-2 border-grey-ghoose border-dashed ': !{ showAccordion2 },
+            'border-b-2 border-grey-ghoose border-dashed': { showAccordion2 }
+          })}
+          sx={{ backgroundColor: 'inherit', boxShadow: '0px 0px 0px 0px #ffffffe6', Opacity: '0.1' }}
+        >
           <AccordionSummary
             expandIcon={
               showAccordion2 ? (
@@ -120,7 +91,7 @@ const PlatformCard = ({ title, name, description, subTitle }: IProps) => {
             </Typography>
           </AccordionSummary>
           <AccordionDetails
-            className={` px-10 h-[100px] overflow-y-scroll scroll-right w-[100%] border-b-2 border-grey-ghoose border-dashed bg-Geyser  ${
+            className={` px-10 h-[100px] overflow-y-scroll scroll-right w-[100%] bg-Geyser  ${
               showAccordion2 ? '' : 'hidden'
             }`}
           >
