8eec3d468be05164088c66bf02915bd67ac4c9dc
abubakarafzal444 <abubakarafzal444@gmail.com>
2023-11-01 08:30:59 +0500

    minor fixes in cards designs

--- src/libs/api/services/CompanyService.ts
+++ src/libs/api/services/CompanyService.ts
@@ -19,354 +19,325 @@ import type { CancelablePromise } from '../core/CancelablePromise';
 import type { BaseHttpRequest } from '../core/BaseHttpRequest';
 
 export class CompanyService {
+  constructor(public readonly httpRequest: BaseHttpRequest) {}
 
-    constructor(public readonly httpRequest: BaseHttpRequest) {}
+  /**
+   * image: registration_document, representative_id_card_front, representative_id_card_back, account_image
+   *
+   * status: 0(新增公司未送審), 1(送審中) 2(審核通過)
+   *
+   * address 存放json dict
+   * @param requestBody
+   * @returns Company
+   * @throws ApiError
+   */
+  public companyCreate(requestBody: ExtendedCompany): CancelablePromise<Company> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/company/',
+      body: requestBody,
+      mediaType: 'application/multi-part/form-data'
+    });
+  }
 
-    /**
-     * image: registration_document, representative_id_card_front, representative_id_card_back, account_image
-     *
-     * status: 0(新增公司未送審), 1(送審中) 2(審核通過)
-     *
-     * address 存放json dict
-     * @param requestBody
-     * @returns Company
-     * @throws ApiError
-     */
-    public companyCreate(
-        requestBody: ExtendedCompany,
-    ): CancelablePromise<Company> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/company/',
-            body: requestBody,
-            mediaType: 'application/multi-part/form-data',
-        });
-    }
+  /**
+   * image: registration_document, representative_id_card_front, representative_id_card_back, account_image
+   *
+   * status: 0(新增公司未送審), 1(送審中) 2(審核通過)
+   *
+   * address 存放json dict
+   * @param id
+   * @returns Company
+   * @throws ApiError
+   */
+  public companyRetrieve(id: number): CancelablePromise<Company> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/{id}/',
+      path: {
+        id: id
+      }
+    });
+  }
 
-    /**
-     * image: registration_document, representative_id_card_front, representative_id_card_back, account_image
-     *
-     * status: 0(新增公司未送審), 1(送審中) 2(審核通過)
-     *
-     * address 存放json dict
-     * @param id
-     * @returns Company
-     * @throws ApiError
-     */
-    public companyRetrieve(
-        id: number,
-    ): CancelablePromise<Company> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/{id}/',
-            path: {
-                'id': id,
-            },
-        });
-    }
+  /**
+   * image: registration_document, representative_id_card_front, representative_id_card_back, account_image
+   *
+   * status: 0(新增公司未送審), 1(送審中) 2(審核通過)
+   *
+   * address 存放json dict
+   * @param id
+   * @param requestBody
+   * @returns Company
+   * @throws ApiError
+   */
+  public companyPartialUpdate(id: number, requestBody?: PatchedExtendedCompany): CancelablePromise<Company> {
+    return this.httpRequest.request({
+      method: 'PATCH',
+      url: '/company/{id}/',
+      path: {
+        id: id
+      },
+      body: requestBody,
+      mediaType: 'application/multi-part/form-data'
+    });
+  }
 
-    /**
-     * image: registration_document, representative_id_card_front, representative_id_card_back, account_image
-     *
-     * status: 0(新增公司未送審), 1(送審中) 2(審核通過)
-     *
-     * address 存放json dict
-     * @param id
-     * @param requestBody
-     * @returns Company
-     * @throws ApiError
-     */
-    public companyPartialUpdate(
-        id: number,
-        requestBody?: PatchedExtendedCompany,
-    ): CancelablePromise<Company> {
-        return this.httpRequest.request({
-            method: 'PATCH',
-            url: '/company/{id}/',
-            path: {
-                'id': id,
-            },
-            body: requestBody,
-            mediaType: 'application/multi-part/form-data',
-        });
-    }
+  /**
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyCheckUserStatusRetrieve(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/check_user_status/'
+    });
+  }
 
-    /**
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyCheckUserStatusRetrieve(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/check_user_status/',
-        });
-    }
+  /**
+   * 網址上帶的id是員工列表回傳裡的id 不是user
+   *
+   * status 0尚未驗證 1驗證 2凍結
+   * @param page A page number within the paginated result set.
+   * @returns PaginatedEmployeeList
+   * @throws ApiError
+   */
+  public companyEmployeeList(page?: number): CancelablePromise<PaginatedEmployeeList> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/employee/',
+      query: {
+        page: page
+      }
+    });
+  }
 
-    /**
-     * 網址上帶的id是員工列表回傳裡的id 不是user
-     *
-     * status 0尚未驗證 1驗證 2凍結
-     * @param page A page number within the paginated result set.
-     * @returns PaginatedEmployeeList
-     * @throws ApiError
-     */
-    public companyEmployeeList(
-        page?: number,
-    ): CancelablePromise<PaginatedEmployeeList> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/employee/',
-            query: {
-                'page': page,
-            },
-        });
-    }
+  /**
+   * 網址上帶的id是員工列表回傳裡的id 不是user
+   *
+   * status 0尚未驗證 1驗證 2凍結
+   * @param requestBody
+   * @returns Employee
+   * @throws ApiError
+   */
+  public companyEmployeeCreate(requestBody: ExtendEmployee): CancelablePromise<Employee> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/company/employee/',
+      body: requestBody,
+      mediaType: 'application/json'
+    });
+  }
 
-    /**
-     * 網址上帶的id是員工列表回傳裡的id 不是user
-     *
-     * status 0尚未驗證 1驗證 2凍結
-     * @param requestBody
-     * @returns Employee
-     * @throws ApiError
-     */
-    public companyEmployeeCreate(
-        requestBody: ExtendEmployee,
-    ): CancelablePromise<Employee> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/company/employee/',
-            body: requestBody,
-            mediaType: 'application/json',
-        });
-    }
+  /**
+   * 網址上帶的id是員工列表回傳裡的id 不是user
+   *
+   * status 0尚未驗證 1驗證 2凍結
+   * @param id
+   * @returns Employee
+   * @throws ApiError
+   */
+  public companyEmployeeRetrieve(id: number): CancelablePromise<Employee> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/employee/{id}/',
+      path: {
+        id: id
+      }
+    });
+  }
 
-    /**
-     * 網址上帶的id是員工列表回傳裡的id 不是user
-     *
-     * status 0尚未驗證 1驗證 2凍結
-     * @param id
-     * @returns Employee
-     * @throws ApiError
-     */
-    public companyEmployeeRetrieve(
-        id: number,
-    ): CancelablePromise<Employee> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/employee/{id}/',
-            path: {
-                'id': id,
-            },
-        });
-    }
+  /**
+   * 網址上帶的id是員工列表回傳裡的id 不是user
+   *
+   * status 0尚未驗證 1驗證 2凍結
+   * @param id
+   * @param requestBody
+   * @returns Employee
+   * @throws ApiError
+   */
+  public companyEmployeePartialUpdate(id: number, requestBody?: PatchedEmployeesPatch): CancelablePromise<Employee> {
+    return this.httpRequest.request({
+      method: 'PATCH',
+      url: '/company/employee/{id}/',
+      path: {
+        id: id
+      },
+      body: requestBody,
+      //   mediaType: 'application/json'
+      mediaType: 'application/multi-part/form-data'
+    });
+  }
 
-    /**
-     * 網址上帶的id是員工列表回傳裡的id 不是user
-     *
-     * status 0尚未驗證 1驗證 2凍結
-     * @param id
-     * @param requestBody
-     * @returns Employee
-     * @throws ApiError
-     */
-    public companyEmployeePartialUpdate(
-        id: number,
-        requestBody?: PatchedEmployeesPatch,
-    ): CancelablePromise<Employee> {
-        return this.httpRequest.request({
-            method: 'PATCH',
-            url: '/company/employee/{id}/',
-            path: {
-                'id': id,
-            },
-            body: requestBody,
-            mediaType: 'application/json',
-        });
-    }
+  /**
+   * 網址上帶的id是員工列表回傳裡的id 不是user
+   *
+   * status 0尚未驗證 1驗證 2凍結
+   * @param id
+   * @returns void
+   * @throws ApiError
+   */
+  public companyEmployeeDestroy(id: number): CancelablePromise<void> {
+    return this.httpRequest.request({
+      method: 'DELETE',
+      url: '/company/employee/{id}/',
+      path: {
+        id: id
+      }
+    });
+  }
 
-    /**
-     * 網址上帶的id是員工列表回傳裡的id 不是user
-     *
-     * status 0尚未驗證 1驗證 2凍結
-     * @param id
-     * @returns void
-     * @throws ApiError
-     */
-    public companyEmployeeDestroy(
-        id: number,
-    ): CancelablePromise<void> {
-        return this.httpRequest.request({
-            method: 'DELETE',
-            url: '/company/employee/{id}/',
-            path: {
-                'id': id,
-            },
-        });
-    }
+  /**
+   * @param page A page number within the paginated result set.
+   * @returns PaginatedGroupList
+   * @throws ApiError
+   */
+  public companyGroupList(page?: number): CancelablePromise<PaginatedGroupList> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/group/',
+      query: {
+        page: page
+      }
+    });
+  }
 
-    /**
-     * @param page A page number within the paginated result set.
-     * @returns PaginatedGroupList
-     * @throws ApiError
-     */
-    public companyGroupList(
-        page?: number,
-    ): CancelablePromise<PaginatedGroupList> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/group/',
-            query: {
-                'page': page,
-            },
-        });
-    }
+  /**
+   * List all snippets, or create a new snippet.
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyLevelRetrieve(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/level/'
+    });
+  }
 
-    /**
-     * List all snippets, or create a new snippet.
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyLevelRetrieve(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/level/',
-        });
-    }
+  /**
+   * List all snippets, or create a new snippet.
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyLevelCreate(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/company/level/'
+    });
+  }
 
-    /**
-     * List all snippets, or create a new snippet.
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyLevelCreate(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/company/level/',
-        });
-    }
+  /**
+   * List all snippets, or create a new snippet.
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyLevelSetRetrieve(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/level_set/'
+    });
+  }
 
-    /**
-     * List all snippets, or create a new snippet.
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyLevelSetRetrieve(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/level_set/',
-        });
-    }
+  /**
+   * List all snippets, or create a new snippet.
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyLevelSetCreate(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/company/level_set/'
+    });
+  }
 
-    /**
-     * List all snippets, or create a new snippet.
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyLevelSetCreate(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/company/level_set/',
-        });
-    }
+  /**
+   * @param page A page number within the paginated result set.
+   * @returns PaginatedPlanList
+   * @throws ApiError
+   */
+  public companyPlanList(page?: number): CancelablePromise<PaginatedPlanList> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/plan/',
+      query: {
+        page: page
+      }
+    });
+  }
 
-    /**
-     * @param page A page number within the paginated result set.
-     * @returns PaginatedPlanList
-     * @throws ApiError
-     */
-    public companyPlanList(
-        page?: number,
-    ): CancelablePromise<PaginatedPlanList> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/plan/',
-            query: {
-                'page': page,
-            },
-        });
-    }
+  /**
+   * @param requestBody
+   * @returns Plan
+   * @throws ApiError
+   */
+  public companyPlanCreate(requestBody?: Plan): CancelablePromise<Plan> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/company/plan/',
+      body: requestBody,
+      mediaType: 'application/json'
+    });
+  }
 
-    /**
-     * @param requestBody
-     * @returns Plan
-     * @throws ApiError
-     */
-    public companyPlanCreate(
-        requestBody?: Plan,
-    ): CancelablePromise<Plan> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/company/plan/',
-            body: requestBody,
-            mediaType: 'application/json',
-        });
-    }
+  /**
+   * @param page A page number within the paginated result set.
+   * @returns PaginatedPlanRecordList
+   * @throws ApiError
+   */
+  public companyPlanRecordList(page?: number): CancelablePromise<PaginatedPlanRecordList> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/plan_record/',
+      query: {
+        page: page
+      }
+    });
+  }
 
-    /**
-     * @param page A page number within the paginated result set.
-     * @returns PaginatedPlanRecordList
-     * @throws ApiError
-     */
-    public companyPlanRecordList(
-        page?: number,
-    ): CancelablePromise<PaginatedPlanRecordList> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/plan_record/',
-            query: {
-                'page': page,
-            },
-        });
-    }
+  /**
+   * @param requestBody
+   * @returns PlanRecord
+   * @throws ApiError
+   */
+  public companyPlanRecordCreate(requestBody: PlanRecord): CancelablePromise<PlanRecord> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/company/plan_record/',
+      body: requestBody,
+      mediaType: 'application/json'
+    });
+  }
 
-    /**
-     * @param requestBody
-     * @returns PlanRecord
-     * @throws ApiError
-     */
-    public companyPlanRecordCreate(
-        requestBody: PlanRecord,
-    ): CancelablePromise<PlanRecord> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/company/plan_record/',
-            body: requestBody,
-            mediaType: 'application/json',
-        });
-    }
+  /**
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyReviewPassReqRetrieve(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/review_pass_req/'
+    });
+  }
 
-    /**
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyReviewPassReqRetrieve(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/review_pass_req/',
-        });
-    }
+  /**
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyUserRetrieve(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'GET',
+      url: '/company/user/'
+    });
+  }
 
-    /**
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyUserRetrieve(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'GET',
-            url: '/company/user/',
-        });
-    }
-
-    /**
-     * @returns any No response body
-     * @throws ApiError
-     */
-    public companyUserCreate(): CancelablePromise<any> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/company/user/',
-        });
-    }
-
+  /**
+   * @returns any No response body
+   * @throws ApiError
+   */
+  public companyUserCreate(): CancelablePromise<any> {
+    return this.httpRequest.request({
+      method: 'POST',
+      url: '/company/user/'
+    });
+  }
 }
--- src/store/employee.ts
+++ src/store/employee.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-import { Employee } from '@/libs/api';
+import { Employee, PatchedEmployeesPatch } from '@/libs/api';
 import apiClient from '@/libs/api/client';
 
 import { runTask } from './modal';
@@ -10,7 +10,7 @@ type EmployeeState = {
   getEmployeeList: (...args: Parameters<typeof apiClient.company.companyEmployeeList>) => void;
   selectedEmployee?: Employee;
   getSelectedEmployee: (...args: Parameters<typeof apiClient.company.companyEmployeeRetrieve>) => void;
-  updateEmployeeDetails: (...args: Parameters<typeof apiClient.company.companyEmployeePartialUpdate>) => void;
+  updateEmployeeDetails: (id: number, companyData?: FormData) => void;
 };
 
 export const useEmployeeStore = create<EmployeeState>((set) => ({
@@ -28,9 +28,9 @@ export const useEmployeeStore = create<EmployeeState>((set) => ({
       set({ selectedEmployee: response });
     });
   },
-  updateEmployeeDetails: async (...args) => {
+  updateEmployeeDetails: async (id, employeeData) => {
     await runTask(async () => {
-      const response = await apiClient.company.companyEmployeePartialUpdate(...args);
+      const response = await apiClient.company.companyEmployeePartialUpdate(id, employeeData as PatchedEmployeesPatch);
       set({ selectedEmployee: response });
     });
   }
