5c23ffd021394c66b38ec8fbf47e1fe31fc677da
aliraza2u <raza8r@gmail.com>
2023-11-18 17:32:35 +0500

    add layout switch in wishlist page

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 
 import FilterBar from '@/components/FilterBar';
 import Navbar from '@/components/Navbar';
+import LayoutSwitch from '@/components/Products/LayoutSwitch';
 import WishListProdCard from '@/components/WishListProdCard';
 import { WatchList } from '@/libs/api';
 import { useWishListStore } from '@/store/wishList';
@@ -17,6 +18,7 @@ const WishList = () => {
   return (
     <div className="bg-[url('../public/images/wishlist/background.png')] bg-no-repeat bg-cover py-10 h-screen px-5 pr-8">
       <Navbar className="mb-8" />
+      <LayoutSwitch />
       <FilterBar />
       <div className="grid grid-cols-2 gap-6.5 overflow-y-scroll yellowScroll overflow-x-hidden pr-5 max-w-[1680px] 3xl:max-w-full mx-auto 3xl:mx-2">
         {wishList.map((product: WatchList) => {
