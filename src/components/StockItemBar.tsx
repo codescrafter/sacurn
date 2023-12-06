c3901a069b374d62edcdb67061e9951f0829ea57
eric.lin <seekingfor520@gmail.com>
2023-12-06 16:49:48 +0800

    Show stock info on SignatureConfirmationModal when product off shelf

--- src/components/StockItemBar.tsx
+++ src/components/StockItemBar.tsx
@@ -2,17 +2,19 @@ import React, { useMemo } from 'react';
 
 import { Order } from '@/libs/api';
 import { ModalType, useModalStore } from '@/store/modal';
-import { useStockListStore } from '@/store/stockList';
+import { StockItem, useStockListStore } from '@/store/stockList';
 import { formatNumberByComma } from '@/util/helper';
 
 import Button from './Button';
 
 interface StockItemProps {
+  stockItem: StockItem;
   order: Order;
   number: number;
 }
 
-const StockItemBar = ({ order, number }: StockItemProps) => {
+const StockItemBar = (props: StockItemProps) => {
+  const { stockItem, order, number } = props;
   const open = useModalStore((state) => state.open);
   const updateStockOffShelve = useStockListStore((state) => state.updateStockOffShelve);
 
@@ -27,10 +29,12 @@ const StockItemBar = ({ order, number }: StockItemProps) => {
           <div>
             <div className="flex justify-between pt-10 pb-15">
               <div className="flex flex-col gap-4 text-dark-grey">
+                <p>Vintage</p>
                 <p>平均單價</p>
                 <p>持有數量</p>
               </div>
               <div className="flex flex-col gap-4 text-right text-xl font-bold text-black">
+                <p>{stockItem.quantity} 噸</p>
                 <p>$ {order.price}</p>
                 <p>{order.quantity} 噸</p>
               </div>
@@ -44,6 +48,7 @@ const StockItemBar = ({ order, number }: StockItemProps) => {
               <div className="flex flex-col gap-4 text-right text-xl font-bold text-black">
                 <p>{order.quantity} 噸</p>
                 <p>{order.price} 元</p>
+                <p>最低下單量 {order.min_order_quantity} 噸</p>
               </div>
             </div>
           </div>
